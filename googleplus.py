df7971fee9b2504b004a946b91666ecb50b5c325
Ryan Barrett <git@ryanb.org>
2012-01-29 10:43:00 -0800

    switch back to normal plus.me oauth scope

--- googleplus.py
+++ googleplus.py
@@ -36,13 +36,12 @@ HARD_CODED_DEST = 'WordPressSite'
 
 # client id and secret aren't stored in the datastore like FacebookApp since
 # it's hard to have the datastore ready in unit tests at module load time.
-with open('oauth_client_secret') as oauth_client_secret_file:
-  with open('oauth_scopes') as oauth_scopes_file:
-    plus_api = OAuth2Decorator(
-      client_id='1029605954231.apps.googleusercontent.com',
-      client_secret=oauth_client_secret_file.read().strip(),
-      scope=oauth_scopes_file.read().strip(),
-      )
+with open('oauth_client_secret') as f:
+  plus_api = OAuth2Decorator(
+    client_id='1029605954231.apps.googleusercontent.com',
+    client_secret=f.read().strip(),
+    scope='https://www.googleapis.com/auth/plus.me',
+    )
 
 
 class GooglePlusService(db.Model):
