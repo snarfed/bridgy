30e7f0af66ea9eab85e49934c729fdf95ba58ecc
Ryan Barrett <git@ryanb.org>
2013-11-21 08:18:27 -0800

    use webmention-dev namespace while testing

--- appengine_config.py
+++ appengine_config.py
@@ -1,1 +1,10 @@
-activitystreams/oauth_dropins/appengine_config.py
+"""Bridgy App Engine config.
+"""
+
+from google.appengine.api import namespace_manager
+
+from activitystreams.appengine_config import *
+
+# Called only if the current namespace is not set.
+def namespace_manager_default_namespace_for_request():
+  return 'webmention-dev'
