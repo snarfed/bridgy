c011a5aaba4f52df0c2ab33cdab67c7a00f79daf
Ryan Barrett <git@ryanb.org>
2013-12-04 07:46:02 -0800

    drop the namespace

--- appengine_config.py
+++ appengine_config.py
@@ -5,6 +5,8 @@ from google.appengine.api import namespace_manager
 
 from activitystreams.appengine_config import *
 
+# I used a namespace for a while when I had both versions deployed, but not any
+# more; I cleared out the old v1 datastore entities.
 # Called only if the current namespace is not set.
-def namespace_manager_default_namespace_for_request():
-  return 'webmention-dev'
+# def namespace_manager_default_namespace_for_request():
+#   return 'webmention-dev'
