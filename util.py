3369c0bbcd3091d6c3a648075cceb779f3cc5cf6
Ryan Barrett <git@ryanb.org>
2013-12-08 08:06:06 -0800

    expand comments in fragment source

--- util.py
+++ util.py
@@ -13,7 +13,7 @@ POLL_TASK_DATETIME_FORMAT = '%Y-%m-%d-%H-%M-%S'
 def added_source_redirect(handler, source):
   """Redirects to the dashboard after adding a source.
   """
-  uri = '/?added=%s#source-%s-%s' % (source.key(), source.DISPLAY_NAME,
+  uri = '/?added=%s#%s-%s' % (source.key(), source.DISPLAY_NAME,
                                      source.key().name())
   uri = add_query_params(uri, [('msg', msg) for msg in handler.messages])
   handler.redirect(uri)
