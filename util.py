b68e75f7cd9d2ef942d156a62d12681c1baa2c53
Ryan Barrett <git@ryanb.org>
2014-01-02 08:21:26 -0800

    wrap webutil.util.tag_uri and hard-code the year to 2013
    Needed because I originally generated tag URIs with the current year, which resulted in different URIs for the same objects when the year changed. :/

--- util.py
+++ util.py
@@ -6,6 +6,9 @@ import urlparse
 from google.appengine.api import taskqueue
 
 import webapp2
+import activitystreams.webutil.util
+import activitystreams.oauth_dropins.webutil.util
+import webutil.util
 from webutil.util import *
 
 EPOCH = datetime.datetime.utcfromtimestamp(0)
@@ -29,6 +32,15 @@ def add_poll_task(source, **kwargs):
                         'last_polled': last_polled_str},
                 **kwargs)
 
+
+# Wrap webutil.util.tag_uri and hard-code the year to 2013.
+#
+# Needed because I originally generated tag URIs with the current year, which
+# resulted in different URIs for the same objects when the year changed. :/
+_orig_tag_uri = webutil.util.tag_uri
+webutil.util.tag_uri = lambda domain, name: _orig_tag_uri(domain, name, year=2013)
+activitystreams.webutil.util.tag_uri = webutil.util.tag_uri
+activitystreams.oauth_dropins.webutil.util = webutil.util.tag_uri
 
 class Handler(webapp2.RequestHandler):
   """Includes misc request handler utilities.
