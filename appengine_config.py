543ec71a2a562b268d54cc432c73c52b75fbd5a7
Ryan Barrett <git@ryanb.org>
2014-02-03 11:00:08 -0800

    turn off ndb's per instance cache in the frontend as well as the backend
    #53

--- appengine_config.py
+++ appengine_config.py
@@ -5,6 +5,13 @@ from google.appengine.api import namespace_manager
 
 from activitystreams.appengine_config import *
 
+# turn off ndb's in-process cache. i'd love to use it, but the frontends
+# constantly hit the memory cap and get killed with it on.
+# https://developers.google.com/appengine/docs/python/ndb/cache
+from google.appengine.ext import ndb
+ndb.Context.default_cache_policy = ndb.Context._cache_policy = \
+    lambda ctx, key: False
+
 # I used a namespace for a while when I had both versions deployed, but not any
 # more; I cleared out the old v1 datastore entities.
 # Called only if the current namespace is not set.
