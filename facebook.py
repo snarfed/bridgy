snarfed/bridgy

commit 38ccdc87b383cb35465e67a2ce7becc204ab2e63

email updated users as well as added and deleted

models.py
@@ -65,26 +65,28 @@ class Site(StringIdModel):
       handler: the current RequestHandler
       **kwargs: passed to new()
     """
-    new = cls.new(handler, **kwargs)
-    existing = new.key.get()
+    site = cls.new(handler, **kwargs)
+    existing = site.key.get()
     if existing:
-      logging.warning('Overwriting %s %s! Old version:\n%s',
-                      existing.label(), existing.key, existing.to_dict())
-      new_msg = "Updated %s. Refresh to see what's new!" % existing.label()
+      site = existing
+      verb = 'Updated'
+      msg = "Updated %s. Refresh to see what's new!"
     else:
-      logging.info('Added %s %s %s', new.label(), new.key.string_id(), new.key)
-      new_msg = "Added %s. Refresh to see what we've found!" % new.label()
-      mail.send_mail(sender='add@brid-gy.appspotmail.com',
-                     to='webmaster@brid.gy',
-                     subject='Added Brid.gy user: %s %s' %
-                     (new.label(), new.key.string_id()),
-                     body='%s/#%s' % (handler.request.host_url, new.dom_id()))
+      verb = 'Added'
+      msg = "Added %s. Refresh to see what we've found!"
 
-    handler.messages = {new_msg}
+    logging.info('%s %s %s %s', verb, site.label(), site.key.string_id(), site.key)
+    mail.send_mail(sender='add@brid-gy.appspotmail.com',
+                   to='webmaster@brid.gy',
+                   subject='%s Bridgy user: %s %s' %
+                   (verb, site.label(), site.key.string_id()),
+                   body='%s/#%s' % (handler.request.host_url, site.dom_id()))
+
+    handler.messages = {msg % site.label()}
 
     # TODO: ugh, *all* of this should be transactional
-    new.put()
-    return new
+    site.put()
+    return site
 
   def dom_id(self):
     """Returns the DOM element id for this site."""

app.py
@@ -209,7 +209,7 @@ class DeleteFinishHandler(util.Handler):
       self.messages.add('Deleted %s. Sorry to see you go!' % source.label())
       mail.send_mail(sender='delete@brid-gy.appspotmail.com',
                      to='webmaster@brid.gy',
-                     subject='Deleted Brid.gy user: %s %s' %
+                     subject='Deleted Bridgy user: %s %s' %
                      (source.label(), source.key.string_id()),
                      body='%s/#%s' % (self.request.host_url, source.dom_id()))
       self.redirect('/?deleted=%s' % source.key.urlsafe())

commit bfa52990bd57e1e607cd3c9c470ee3aa11e89540

start to implement publish/listen distinction
also fix bug introduced in 38ccdc87b383cb35465e67a2ce7becc204ab2e63: actually overwrite source entity when updating.

facebook.py
@@ -1,10 +1,6 @@
 """Facebook API code and datastore model classes.
 
-Permissions needed:
-read_stream for links posted by user
-offline_access for, uh, offline access
-
-TODO: use third_party_id if we ever need to store an fb user id anywhere else.
+TODO: use third_party_id if we ever need to store an FB user id anywhere else.
 
 Example post ID and links
   id: 212038_10100823411129293  [USER-ID]_[POST-ID]
@@ -43,8 +39,10 @@ import webapp2
 
 
 # https://developers.facebook.com/docs/reference/login/
-OAUTH_SCOPES = ('offline_access', 'user_status', 'user_photos', 'user_events',
-                'read_stream')
+LISTEN_OAUTH_SCOPES = ('offline_access', 'user_status', 'user_photos',
+                       'user_events', 'read_stream')
+PUBLISH_OAUTH_SCOPES = ('offline_access', 'publish_actions', 'create_event ',
+                        'rsvp_event')
 
 API_PHOTOS_URL = 'https://graph.facebook.com/me/photos/uploaded'
 API_USER_RSVPS_URL = 'https://graph.facebook.com/me/events'  # returns yes and maybe
@@ -68,12 +66,13 @@ class FacebookPage(models.Source):
   username = ndb.StringProperty()
 
   @staticmethod
-  def new(handler, auth_entity=None):
+  def new(handler, auth_entity=None, **kwargs):
     """Creates and returns a FacebookPage for the logged in user.
 
     Args:
       handler: the current RequestHandler
       auth_entity: oauth_dropins.facebook.FacebookAuth
+      kwargs: property values
     """
     user = json.loads(auth_entity.user_json)
     id = user['id']
@@ -84,7 +83,8 @@ class FacebookPage(models.Source):
                         name=user.get('name'),
                         username=user.get('username'),
                         auth_entity=auth_entity.key,
-                        picture=picture, url=url)
+                        picture=picture, url=url,
+                        **kwargs)
 
   def get(self, url):
     """Simple wrapper around urlopen(). Returns decoded JSON dict."""
@@ -181,7 +181,7 @@ class AddFacebookPage(oauth_facebook.CallbackHandler, util.Handler):
 
 application = webapp2.WSGIApplication([
     ('/facebook/start',oauth_facebook.StartHandler.to('/facebook/add',
-                                                      scopes=OAUTH_SCOPES)),
+                                                      scopes=LISTEN_OAUTH_SCOPES)),
     ('/facebook/add', AddFacebookPage),
     ('/facebook/delete/finish', oauth_facebook.CallbackHandler.to('/delete/finish')),
     ], debug=appengine_config.DEBUG)

googleplus.py
@@ -39,7 +39,7 @@ class GooglePlusPage(models.Source):
   type = ndb.StringProperty(choices=('user', 'page'))
 
   @staticmethod
-  def new(handler, auth_entity=None):
+  def new(handler, auth_entity=None, **kwargs):
     """Creates and returns a GooglePlusPage for the logged in user.
 
     Args:
@@ -61,7 +61,8 @@ class GooglePlusPage(models.Source):
                           url=user['url'],
                           name=user['displayName'],
                           picture=picture,
-                          type=type)
+                          type=type,
+                          **kwargs)
 
   def __getattr__(self, name):
     """Overridden to pass auth_entity to as_googleplus.GooglePlus's ctor."""

instagram.py
@@ -42,7 +42,7 @@ class Instagram(models.Source):
   SHORT_NAME = 'instagram'
 
   @staticmethod
-  def new(handler, auth_entity=None):
+  def new(handler, auth_entity=None, **kwargs):
     """Creates and returns a InstagramPage for the logged in user.
 
     Args:
@@ -55,7 +55,8 @@ class Instagram(models.Source):
                      auth_entity=auth_entity.key,
                      name=user['full_name'],
                      picture=user['profile_picture'],
-                     url='http://instagram.com/' + username)
+                     url='http://instagram.com/' + username,
+                     **kwargs)
 
   def get_activities_response(self, *args, **kwargs):
     """Discard min_id because we still want new comments/likes on old photos."""

models.py
@@ -68,7 +68,6 @@ class Site(StringIdModel):
     site = cls.new(handler, **kwargs)
     existing = site.key.get()
     if existing:
-      site = existing
       verb = 'Updated'
       msg = "Updated %s. Refresh to see what's new!"
     else:
@@ -107,7 +106,7 @@ class Source(Site):
   name = ndb.StringProperty()
   picture = ndb.StringProperty()
 
-  features = ndb.StringProperty(repeated=True, choices=('publish',))
+  features = ndb.StringProperty(repeated=True, choices=('listen', 'publish'))
 
   # points to an oauth-dropins auth entity. The model class should be a subclass
   # of oauth_dropins.BaseAuth.

templates/publish.html
@@ -6,15 +6,18 @@
 <div id="connect" class="row">
 <div class="col-md-4 col-sm-8">
  <form method="post" name="facebook-start" action="/facebook/start">
-  <input name="scope" type="hidden" value="publish_actions" />
+  <input name="scope" type="hidden" value="publish_actions,create_event,rsvp_event" />
+  <input name="state" type="hidden" value="publish" />
   <input type="image" alt="Sign in with Facebook" src="/static/facebook_button.png" />
 </form></div>
 <div class="col-md-4 col-sm-8">
  <form method="post" name="twitter-start" action="/twitter/start">
+  <input name="state" type="hidden" value="publish" />
   <input type="image" alt="Sign in with Twitter" src="/static/twitter_button.png" />
 </form></div>
 <div class="col-md-4 col-sm-8">
  <form method="post" name="instagram-start" action="/instagram/start">
+  <input name="state" type="hidden" value="publish" />
   <input type="image" alt="Sign in with Instagram" src="/static/instagram_button.png" />
 </form></div>
 </div>

twitter.py
@@ -34,7 +34,7 @@ class Twitter(models.Source):
   POLL_FREQUENCY = datetime.timedelta(minutes=10)
 
   @staticmethod
-  def new(handler, auth_entity=None):
+  def new(handler, auth_entity=None, **kwargs):
     """Creates and returns a Twitter entity.
 
     Args:
@@ -49,7 +49,8 @@ class Twitter(models.Source):
                    auth_entity=auth_entity.key,
                    url=Twitter.user_url(user['screen_name']),
                    name=user['name'],
-                   picture=picture)
+                   picture=picture,
+                   **kwargs)
 
   def get_like(self, activity_user_id, activity_id, like_user_id):
     """Returns an ActivityStreams 'like' activity object for a favorite.

models_test.py
@@ -64,8 +64,8 @@ class ResponseTest(testutil.ModelsTest):
 
 class SourceTest(testutil.HandlerTest):
 
-  def _test_create_new(self):
-    FakeSource.create_new(self.handler)
+  def _test_create_new(self, **kwargs):
+    FakeSource.create_new(self.handler, **kwargs)
     self.assertEqual(1, FakeSource.query().count())
 
     tasks = self.taskqueue_stub.GetTasks('poll')
@@ -85,10 +85,13 @@ class SourceTest(testutil.HandlerTest):
 
   def test_create_new_already_exists(self):
     FakeSource.new(None).put()
+    self.assert_equals([], FakeSource.query().get().features)
+
     FakeSource.string_id_counter -= 1
-    self._test_create_new()
-    msg = "Updated fake (FakeSource). Refresh to see what's new!"
-    self.assert_equals({msg}, self.handler.messages)
+    self._test_create_new(features=('publish',))
+    self.assert_equals(['publish'], FakeSource.query().get().features)
+    self.assert_equals({"Updated fake (FakeSource). Refresh to see what's new!"},
+                       self.handler.messages)
 
   def test_get_post(self):
     post = {'verb': 'post', 'object': {'objectType': 'note', 'content': 'asdf'}}