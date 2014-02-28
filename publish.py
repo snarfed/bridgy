548dd361472fe932543b29449fd1ebed5a7704a0
Ryan Barrett <git@ryanb.org>
2014-02-27 20:12:53 -0800

    checkpoint publishing. works for posting status updates to FB, maybe more.

--- publish.py
+++ publish.py
@@ -16,23 +16,32 @@ Example response:
     HTTP/1.1 202 Accepted
 
     http://brid.gy/webmentions/222
+
+Test cmd line:
+
+curl -d 'source=http://localhost/bridgy_fb_post.html&target=http://brid.gy/publish/facebook' http://localhost:8080/publish/webmention
 """
 
 __author__ = ['Ryan Barrett <bridgy@ryanb.org>']
 
 import logging
 import json
+import StringIO
+import sys
 import urllib2
 import urlparse
 from webob import exc
 
 import appengine_config
+from appengine_config import HTTP_TIMEOUT
 
+from activitystreams import microformats2
 from activitystreams.oauth_dropins.webutil import util
 import facebook
 import googleplus
 import instagram
 from mf2py import parser
+import requests
 import twitter
 import webapp2
 
@@ -62,41 +71,73 @@ class WebmentionHandler(webapp2.RequestHandler):
                    subject='Bridgy publish: %s' % self.request.get('target'),
                    body=str(self.request.params))
 
-    source = util.get_required_param(self, 'source')
-    target = util.get_required_param(self, 'target')
+    source_url = util.get_required_param(self, 'source')
+    target_url = util.get_required_param(self, 'target')
 
-    # parse and validate source URL
+    # parse and validate target URL
     try:
-      parsed = urlparse.urlparse(target)
-    except BaseException, e:
-      msg = 'Could not parse target URL %s' % target
-      logging.exception(msg, e)
-      return self.error(msg)
+      parsed = urlparse.urlparse(target_url)
+    except BaseException:
+      return self.error(msg, 'Could not parse target URL %s' % target_url)
 
     domain = parsed.netloc
-    # src_domain = parsed.netloc
-    # if src_domain.startswith('www.'):
-    #   src_domain = src_domain[4:]
-    # app_domain = appengine_config.HOST
-    # if app_domain.startswith('www.'):
-    #   app_domain = app_domain[4:]
-
     path_parts = parsed.path.rsplit('/', 1)
-    target_cls = SOURCES.get(path_parts[-1])
-    logging.info('@ %s', [domain, path_parts, target_cls])
+    source_cls = SOURCES.get(path_parts[-1])
     if (domain not in ('brid.gy', 'www.brid.gy') or len(path_parts) != 2 or
-        path_parts[0] != '/publish' or not target_cls):
+        path_parts[0] != '/publish' or not source_cls):
       return self.error('Target must be brid.gy/publish/{facebook,twitter}')
 
-    if target_cls not in (facebook.FacebookPage, twitter.Twitter):
-      return self.error('Sorry, %s is not yet supported.' % target_cls.AS_CLASS.NAME)
+    if source_cls not in (facebook.FacebookPage, twitter.Twitter):
+      return self.error('Sorry, %s is not yet supported.' % source_cls.AS_CLASS.NAME)
 
-    # TODO: timeout? fetch myself?
-    # urllib2.urlopen(url, timeout=appengine_config.HTTP_TIMEOUT)
-    data = parser.Parser(url=source).to_dict()
-    logging.info(str(data))
+    # validate, fetch, and parse source
+    try:
+      parsed = urlparse.urlparse(source_url)
+    except BaseException:
+      return self.error(msg, 'Could not parse source URL %s' % source)
+
+    domain = parsed.netloc
+    # When debugging locally, use snarfed.org for localhost webmentions
+    if appengine_config.DEBUG and domain == 'localhost':
+      domain = 'snarfed.org'
+
+    # look up source by domain
+    source = source_cls.query().filter(source_cls.domain == domain).get()
+    if not source:
+      return self.error(
+        "Could not find %(type)s account for %(domain)s. Check that you're signed up "
+        "for Bridgy and that your %(type)s account has %(domain)s in in its profile's "
+        "'web site' or 'link' field ." %
+        {'type': source_cls.AS_CLASS.NAME, 'domain': domain})
+
+    # try:
+    #   resp = requests.get(source, allow_redirects=True, timeout=HTTP_TIMEOUT)
+    # except BaseException:
+    #   return self.error(msg, 'Could not fetch source URL %s' % source)
+    # TODO(timeout)
+    # data = parser.Parser(file=StringIO.StringIO(resp.text)).to_dict()
+
+    # TODO: fetch myself? mf2py doesn't work when I give it a StringIO though.
+    data = parser.Parser(source_url).to_dict()
+    logging.info('@ %s', data)
+    items = data.get('items', [])
+    if not items or not items[0]:
+      self.error('No mf2 data found in %s. Found: %s', source_url, data)
+
+    contents = items[0].get('properties', {}).get('content', [])
+    if not contents or not contents[0] or not contents[0].get('value'):
+      self.error('Could not find e-content in %s. Found: %s' % (source_url, data))
+
+    obj = microformats2.json_to_object(items[0])
+    # TODO: make prettier?
+    obj['content'] += '\n\n%s' % source_url
+    logging.info('@ %s', obj)
+
+    # resp = source.as_source.create(obj)
+    # self.response.write(json.dumps(resp))
 
   def error(self, error, status=400):
+    logging.error(error, exc_info=sys.exc_info())
     self.response.set_status(status)
     self.response.write(json.dumps({'error': error}))
 
