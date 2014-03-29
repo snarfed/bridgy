e61c4ad8745b4337e66b6e71117d86a799bc2f54
Ryan Barrett <git@ryanb.org>
2014-03-28 21:59:13 -0700

    add mapreduce to prune the Response.activity_json field. fixes #68

---
+++ mapreduces.py
@@ -0,0 +1,27 @@
+"""Mapreduces. Right now just maintennance tasks, no production code.
+
+https://developers.google.com/appengine/docs/python/dataprocessing/
+
+Best guides I found for writing a datastore mapreduce and tuning it in prod:
+https://code.google.com/p/appengine-mapreduce/wiki/GettingStartedInPython
+http://code.google.com/p/appengine-mapreduce/wiki/InstancesQueuesShardsAndSlices
+"""
+
+import gc
+import json
+
+from mapreduce import operation as op
+import util
+import webapp2
+
+
+def prune_activity_json(response):
+  """Prune the Response.activity_json property.
+
+  Background: https://github.com/snarfed/bridgy/issues/68
+  """
+  response.activity_json = json.dumps(util.prune_activity(
+      json.loads(response.activity_json)))
+  # helps avoid hitting the instance memory limit
+  gc.collect()
+  yield op.db.Put(response)
