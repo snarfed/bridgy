0857a7aff38bc39874cb9561d91a06d4fe2d3f3d
Ryan Barrett <git@ryanb.org>
2014-04-27 18:07:51 -0700

    blog webmentions: first pass at wordpress create_comment. it works!

--- wordpress_rest.py
+++ wordpress_rest.py
@@ -1,4 +1,32 @@
 """WordPress REST API (including WordPress.com) hosted blog implementation.
+
+https://developer.wordpress.com/docs/api/
+create returns id, can lookup by id
+
+disqus for tumblr
+http://disqus.com/api/docs/
+http://disqus.com/api/docs/posts/create/
+https://github.com/disqus/DISQUS-API-Recipes/blob/master/snippets/php/create-guest-comment.php
+http://help.disqus.com/customer/portal/articles/466253-what-html-tags-are-allowed-within-comments-
+create returns id, can lookup by id w/getContext?
+
+test:
+curl localhost:8080/webmention/wordpress \
+  -d 'source=http://localhost/response.html&target=http://ryandc.wordpress.com/2013/03/24/mac-os-x/'
+
+
+blogger
+https://developers.google.com/blogger/docs/2.0/developers_guide_protocol
+https://support.google.com/blogger/answer/42064?hl=en
+create comment:
+https://developers.google.com/blogger/docs/2.0/developers_guide_protocol#CreatingComments
+
+
+
+superfeedr:
+https://superfeedr.com/users/snarfed
+http://documentation.superfeedr.com/subscribers.html
+http://documentation.superfeedr.com/schema.html
 """
 
 __author__ = ['Ryan Barrett <bridgy@ryanb.org>']
@@ -8,8 +36,11 @@ import datetime
 import json
 import logging
 import os
+import urllib
+import urlparse
 
 import appengine_config
+from appengine_config import HTTP_TIMEOUT
 
 from activitystreams.oauth_dropins import wordpress_rest as oauth_wordpress
 import models
@@ -18,6 +49,8 @@ import util
 from google.appengine.ext import ndb
 import webapp2
 
+API_CREATE_COMMENT_URL = 'https://public-api.wordpress.com/rest/v1/sites/%s/posts/%d/replies/new?pretty=1'
+API_POST_SLUG_URL = 'https://public-api.wordpress.com/rest/v1/sites/%s/posts/slug:%s?pretty=1'
 
 FakeAsClass = collections.namedtuple('FakeAsClass', ('NAME',))
 
@@ -53,6 +86,44 @@ class WordPress(models.Source):
     Returns: (string url, string domain, True)
     """
     return auth_entity.blog_url, auth_entity.key.id(), True
+
+
+  def create_comment(self, post_url, author_name, author_url, content):
+    """Creates a new comment in the source silo.
+
+    Must be implemented by subclasses.
+
+    Args:
+      post_url: string
+      author_name: string
+      author_url: string
+      content: string
+
+    Returns: JSON response dict with 'id' and other fields
+    """
+    auth_entity = self.auth_entity.get()
+
+    # extract the post's slug and look up its post id
+    path = urlparse.urlparse(post_url).path
+    if path.endswith('/'):
+      path = path[:-1]
+    slug = path.split('/')[-1]
+    try:
+      post_id = int(slug)
+    except ValueError:
+      url = API_POST_SLUG_URL % (auth_entity.key.id(), slug)
+      resp = auth_entity.urlopen(url).read()
+      post_id = json.loads(resp)['ID']
+
+    # create the comment
+    url = API_CREATE_COMMENT_URL % (auth_entity.key.id(), post_id)
+    data = {'content': '<a href="%s">%s</a>: %s\n\n<a href="">via foo</a>' %
+            (author_url, author_name, content)}
+    resp = auth_entity.urlopen(url, data=urllib.urlencode(data)).read()
+    resp = json.loads(resp)
+    resp['id'] = resp.pop('ID', None)
+    return resp
+
 
 class AddWordPress(oauth_wordpress.CallbackHandler, util.Handler):
   def finish(self, auth_entity, state=None):
