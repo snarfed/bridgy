664bc4066fd32f34e2ca1480bd8db8dc18a36295
Ryan Barrett <git@ryanb.org>
2014-07-28 23:04:39 -0700

    add /admin/responses dashboard. fixes #239

---
+++ admin.py
@@ -0,0 +1,55 @@
+"""Renders admin pages for ops and other management tasks.
+
+Currently just /admin/responses, which shows active responses with tasks that
+haven't completed yet.
+"""
+
+import datetime
+import json
+
+import appengine_config
+from activitystreams.oauth_dropins.webutil import handlers
+import facebook
+import googleplus
+from models import Response
+import instagram
+import twitter
+import util
+import webapp2
+
+
+class ResponsesHandler(handlers.TemplateHandler):
+  NUM_RESPONSES = 30
+
+  def template_file(self):
+    return 'templates/admin_responses.html'
+
+  def template_vars(self):
+    responses = []
+
+    # Find the most recently propagated responses with error URLs
+    for r in Response.query().order(-Response.updated):
+      if (len(responses) >= self.NUM_RESPONSES or
+          r.updated < datetime.datetime.now() - datetime.timedelta(hours=3)):
+        break
+      elif not r.error:
+        continue
+
+      # r.source = r.source.get()
+      r.links = [util.pretty_link(u, new_tab=True) for u in r.error + r.failed]
+      r.response = json.loads(r.response_json)
+      r.activities = [json.loads(a) for a in r.activities_json]
+
+      responses.append(r)
+
+    responses.sort(key=lambda r: (r.source, r.activities, r.response))
+    return {'responses': responses}
+
+
+application = webapp2.WSGIApplication([
+    ('/admin/responses', ResponsesHandler),
+    ], debug=appengine_config.DEBUG)
+
+
+if __name__ == '__main__':
+  main()
