e4ab5b2336d1bb7b7aafd71ecc62ccf537cb3044
Ryan Barrett <git@ryanb.org>
2014-09-10 08:00:00 -0700

    more /admin/responses bug fixes

--- admin.py
+++ admin.py
@@ -22,35 +22,35 @@ import webapp2
 
 
 class ResponsesHandler(handlers.TemplateHandler):
-  NUM_RESPONSES = 30
+  NUM_ENTITIES = 30
 
   def template_file(self):
     return 'templates/admin_responses.html'
 
   def template_vars(self):
-    responses = []
+    entities = []
 
     # Find the most recently attempted responses and blog posts with error URLs
-    for e in itertools.chain(*(cls.query().order(-cls.updated)
-                               for cls in (BlogPost, Response))):
-      if (len(responses) >= self.NUM_RESPONSES or
-          e.updated < datetime.datetime.now() - datetime.timedelta(hours=1)):
-        break
-      elif (not e.error and not e.unsent) or e.status == 'complete':
-        continue
+    for cls in BlogPost, Response:
+      for e in cls.query().order(-cls.updated):
+        if (len(entities) >= self.NUM_ENTITIES or
+            e.updated < datetime.datetime.now() - datetime.timedelta(hours=1)):
+          break
+        elif (not e.error and not e.unsent) or e.status == 'complete':
+          continue
 
-      e.links = [util.pretty_link(u, new_tab=True) for u in e.error + e.failed]
-      if e.key.kind() == 'Response':
-        e.response = json.loads(e.response_json)
-        e.activities = [json.loads(a) for a in e.activities_json]
-      else:
-        e.response = {'content': '[BlogPost]'}
-        e.activities = [{'url': e.key.id()}]
+        e.links = [util.pretty_link(u, new_tab=True) for u in e.error + e.failed]
+        if e.key.kind() == 'Response':
+          e.response = json.loads(e.response_json)
+          e.activities = [json.loads(a) for a in e.activities_json]
+        else:
+          e.response = {'content': '[BlogPost]'}
+          e.activities = [{'url': e.key.id()}]
 
-      responses.append(e)
+        entities.append(e)
 
-    responses.sort(key=lambda e: (e.source, e.activities, e.response))
-    return {'responses': responses}
+    entities.sort(key=lambda e: (e.source, e.activities, e.response))
+    return {'responses': entities}
 
 
 class MarkCompleteHandler(util.Handler):
@@ -59,7 +59,7 @@ class MarkCompleteHandler(util.Handler):
                              for u in self.request.params.getall('key'))
     for e in entities:
       e.status = 'complete'
-    ndb.put_multi(responses)
+    ndb.put_multi(entities)
     self.redirect('/admin/responses')
 
 
