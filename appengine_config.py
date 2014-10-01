c6cf2bf5c3aa1ec95084a6aa2f062753e5296c36
Ryan Barrett <git@ryanb.org>
2014-09-30 17:12:45 -0700

    loosen http 400 ereporter blacklist prefix

--- appengine_config.py
+++ appengine_config.py
@@ -57,7 +57,7 @@ class BlacklistingHandler(ereporter.ExceptionRecordingHandler):
     'ConnectionError: HTTPSConnectionPool',
     'DeadlineExceededError',
     'error: An error occured while connecting to the server: Unable to fetch URL:',
-    'HTTPError: HTTP Error 400: Bad Request:',
+    'HTTPError: HTTP Error 400: Bad Request',
     'HTTPError: HTTP Error 404: Not Found',
     'HTTPError: HTTP Error 500: Internal Server Error',
     'HTTPError: HTTP Error 502: Bad Gateway',
