c9d3d6baed3c046f26a13b1d85b7c47e2f8cf9f5
Ryan Barrett <git@ryanb.org>
2015-01-09 14:41:12 -0800

    fix #338

--- app.py
+++ app.py
@@ -256,7 +256,11 @@ class UserHandler(DashboardHandler):
                                   .fetch(10)
       for b in blogposts:
         b.links = self.process_webmention_links(b)
-        b.pretty_url = util.pretty_link(b.key.id(), text=b.feed_item.get('title'),
+        try:
+          text = b.feed_item.get('title')
+        except ValueError:
+          text = None
+        b.pretty_url = util.pretty_link(b.key.id(), text=text,
                                         a_class='original-post', max_length=40,
                                         new_tab=True)
 
