9335b3069a697d1c9152e159c7159be87a03dd21
Ryan Barrett <git@ryanb.org>
2015-03-12 16:31:19 -0700

    temporarily turn off sending updates for changed responses. for #374

--- tasks.py
+++ tasks.py
@@ -254,8 +254,9 @@ class Poll(webapp2.RequestHandler):
       for seen in json.loads(source.seen_responses_cache_json):
         id = seen['id']
         resp = responses.get(id)
-        if resp and not source.as_source.activity_changed(seen, resp, log=True):
-          unchanged_responses.append(seen)
+        # TEMPORARILY discard changed responses. see #374. TODO(ryan): revert
+        if resp:  # and not source.as_source.activity_changed(seen, resp, log=True):
+          unchanged_responses.append(resp)
           del responses[id]
 
     #
