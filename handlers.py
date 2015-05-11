30ba1d0ccb191b2754b23b71e8a7483a562dbda1
Kyle Mahan <kyle.mahan@gmail.com>
2015-05-10 18:08:01 -0700

    use interpret_http_exception for better error logging

--- handlers.py
+++ handlers.py
@@ -91,8 +91,29 @@ class ItemHandler(webapp2.RequestHandler):
       if not post:
         logging.warning('Source post %s not found', post_id)
       return post
-    except:
-      logging.warning('Error fetching source post %s', post_id, exc_info=True)
+    except Exception, e:
+      # use interpret_http_exception to log HTTP errors
+      if not interpret_http_exception(e)[0]:
+        logging.warning(
+          'Error fetching source post %s', post_id, exc_info=True)
+
+  def get_event(self, event_id):
+    """Utility method fetches the original event
+    Args:
+      event_id: string, site-specific event id
+
+    Returns: ActivityStreams object dict
+    """
+    try:
+      event = self.source.get_event(event_id)
+      if not event:
+        logging.warning('Source event %s not found', event_id)
+      return event
+    except Exception, e:
+      # use interpret_http_exception to log HTTP errors
+      if not interpret_http_exception(e)[0]:
+        logging.warning(
+          'Error fetching source event %s', event_id, exc_info=True)
 
   def get(self, type, source_short_name, string_id, *ids):
     source_cls = models.sources.get(source_short_name)
@@ -272,11 +293,9 @@ class RsvpHandler(ItemHandler):
     rsvp = self.source.get_rsvp(self.source.key.string_id(), event_id, user_id)
     if not rsvp:
       return None
-    event = self.source.get_event(event_id)
+    event = self.get_event(event_id)
     if event:
       self.add_original_post_urls(event, rsvp, 'inReplyTo')
-    else:
-      logging.warning('Could not fetch source event %s', event_id)
     return rsvp
 
 
