snarfed/bridgy

commit 168ce81a182ce202ddccc2c4f81ad2325e22d477

split a new Source.poll_status property out of Source.status
for #538

publish.py
@@ -149,8 +149,9 @@ class Handler(webmention.WebmentionHandler):
         {'type': source_cls.GR_CLASS.NAME, 'domain': domain})
 
     for source in sources:
-      logging.info('Source: %s , features %s, status %s' %
-                   (source.bridgy_url(self), source.features, source.status))
+      logging.info('Source: %s , features %s, status %s, poll status %s',
+                   source.bridgy_url(self), source.features, source.status,
+                   source, source.poll_status)
       if source.status != 'disabled' and 'publish' in source.features:
         self.source = source
         break

cron.py
@@ -28,7 +28,7 @@ class ReplacePollTasks(webapp2.RequestHandler):
   def get(self):
     now = datetime.datetime.now()
     queries = [cls.query(Source.features == 'listen',
-                         Source.status.IN(('enabled', 'error')))
+                         Source.status != 'disabled')
                for cls in models.sources.values()]
     for source in itertools.chain(*queries):
       age = now - source.last_poll_attempt

models.py
@@ -63,7 +63,8 @@ class Source(StringIdModel):
   """
   __metaclass__ = SourceMeta
 
-  STATUSES = ('enabled', 'disabled', 'error')
+  STATUSES = ('enabled', 'disabled', 'error')  # 'error' is deprecated
+  POLL_STATUSES = ('ok', 'error', 'polling')
   FEATURES = ('listen', 'publish', 'webmention')
 
   # short name for this site type. used in URLs, etc.
@@ -88,6 +89,7 @@ class Source(StringIdModel):
   created = ndb.DateTimeProperty(auto_now_add=True, required=True)
   url = ndb.StringProperty()
   status = ndb.StringProperty(choices=STATUSES, default='enabled')
+  poll_status = ndb.StringProperty(choices=POLL_STATUSES, default='ok')
   name = ndb.StringProperty()  # full human-readable name
   picture = ndb.StringProperty()
   domains = ndb.StringProperty(repeated=True)
@@ -201,6 +203,10 @@ class Source(StringIdModel):
     source.updates = updates  # because FacebookPage._pre_put_hook uses it
     for name, val in updates.items():
       setattr(source, name, val)
+
+    if source.status == 'error':  # deprecated
+      logging.warning('Resetting status from error to enabled')
+      source.status = 'enabled'
 
     source.put()
     return source

tasks.py
@@ -85,7 +85,7 @@ class Poll(webapp2.RequestHandler):
       source.updates['status'] = 'disabled'
       logging.warning('Disabling source!')
     except:
-      source.updates['status'] = 'error'
+      source.updates['poll_status'] = 'error'
       raise
     finally:
       source = models.Source.put_updates(source)
@@ -170,10 +170,11 @@ class Poll(webapp2.RequestHandler):
       if code == '401':
         msg = 'Unauthorized error: %s' % e
         logging.warning(msg, exc_info=True)
+        source.updates['poll_status'] = 'ok'
         raise models.DisableSource(msg)
       elif code in util.HTTP_RATE_LIMIT_CODES:
         logging.warning('Rate limited. Marking as error and finishing. %s', e)
-        source.updates.update({'status': 'error', 'rate_limited': True})
+        source.updates.update({'poll_status': 'error', 'rate_limited': True})
         return
       elif (code and int(code) / 100 == 5) or util.is_connection_failure(e):
         logging.error('API call failed. Marking as error and finishing. %s: %s\n%s',
@@ -324,7 +325,7 @@ class Poll(webapp2.RequestHandler):
         pruned_responses + unchanged_responses)
 
     source.updates.update({'last_polled': source.last_poll_attempt,
-                           'status': 'enabled'})
+                           'poll_status': 'ok'})
     etag = response.get('etag')
     if etag and etag != source.last_activities_etag:
       logging.debug('Storing new ETag: %s', etag)

templates/social_user.html
@@ -69,7 +69,7 @@ WordPress.com, <a href="/">sign up here!</a>
   {% if source.last_poll_attempt == epoch %}
     Not polled yet.
   {% else %}
-    {% if source.status == "error" %}
+    {% if source.poll_status == "error" %}
      <span title="Error" class="glyphicon glyphicon-exclamation-sign"></span>
     {% endif %}
     Polled

test/test_flickr.py
@@ -69,4 +69,5 @@ class FlickrTest(testutil.ModelsTest):
 
     with self.assertRaises(models.DisableSource):
       poll_task = tasks.Poll()
+      self.flickr.updates = {}
       poll_task.poll(self.flickr)

test/test_tasks.py
@@ -137,6 +137,7 @@ class PollTest(TaskQueueTest):
 
     source = self.sources[0].key.get()
     self.assertEqual(NOW, source.last_polled)
+    self.assertEqual('ok', source.poll_status)
 
     tasks = self.taskqueue_stub.GetTasks('propagate')
     for task in tasks:
@@ -159,7 +160,7 @@ class PollTest(TaskQueueTest):
 
     self.assertRaises(Exception, self.post_task)
     source = self.sources[0].key.get()
-    self.assertEqual('error', source.status)
+    self.assertEqual('error', source.poll_status)
     self.assertEqual(0, len(self.taskqueue_stub.GetTasks('poll')))
 
   def test_poll_silo_500(self):
@@ -169,7 +170,7 @@ class PollTest(TaskQueueTest):
     self.mox.ReplayAll()
 
     self.post_task(expected_status=tasks.ERROR_HTTP_RETURN_CODE)
-    self.assertEqual('error', self.sources[0].key.get().status)
+    self.assertEqual('error', self.sources[0].key.get().poll_status)
 
   def test_poll_silo_deadlines(self):
     """If a silo HTTP request deadlines, we should quietly retry the task."""
@@ -178,16 +179,18 @@ class PollTest(TaskQueueTest):
     self.mox.ReplayAll()
 
     self.post_task(expected_status=tasks.ERROR_HTTP_RETURN_CODE)
-    self.assertEqual('error', self.sources[0].key.get().status)
+    self.assertEqual('error', self.sources[0].key.get().poll_status)
 
   def test_reset_status_to_enabled(self):
-    """After a successful poll, the source status should be set to 'enabled'."""
+    """After a successful poll, status should be set to 'enabled' and 'ok'."""
     self.sources[0].status = 'error'
+    self.sources[0].poll_status = 'error'
     self.sources[0].put()
 
     self.post_task()
     source = self.sources[0].key.get()
     self.assertEqual('enabled', source.status)
+    self.assertEqual('ok', source.poll_status)
 
   def test_original_post_discovery(self):
     """Target URLs should be extracted from attachments, tags, and text."""
@@ -656,6 +659,7 @@ class PollTest(TaskQueueTest):
     self.sources[0].put()
     self.post_task()
     self.assertEqual([], self.taskqueue_stub.GetTasks('poll'))
+    self.assertEqual('enabled', self.sources[0].key.get().status)
 
   def test_disable_source_on_deauthorized(self):
     """If the source raises DisableSource, disable it.
@@ -667,8 +671,7 @@ class PollTest(TaskQueueTest):
     source.status = 'enabled'
     source.put()
     self.post_task()
-    source = source.key.get()
-    self.assertEqual('disabled', source.status)
+    self.assertEqual('disabled', source.key.get().status)
 
   def test_site_specific_disable_sources(self):
     """HTTP 401 and 400 '' for Instagram should disable the source."""
@@ -686,8 +689,7 @@ class PollTest(TaskQueueTest):
         self.mox.ReplayAll()
 
         self.post_task()
-        source = self.sources[0].key.get()
-        self.assertEqual('disabled', source.status)
+        self.assertEqual('disabled', self.sources[0].key.get().status)
 
     finally:
       self.mox.UnsetStubs()
@@ -710,7 +712,7 @@ class PollTest(TaskQueueTest):
 
         self.post_task()
         source = self.sources[0].key.get()
-        self.assertEqual('error', source.status)
+        self.assertEqual('error', source.poll_status)
         self.mox.VerifyAll()
 
         # should have inserted a new poll task

commit d5f1be36e54cdb10f31f3e051a1273bbad8a927f

fix dumb logging bug introduced in 168ce81a182ce202ddccc2c4f81ad2325e22d477

publish.py
@@ -151,7 +151,7 @@ class Handler(webmention.WebmentionHandler):
     for source in sources:
       logging.info('Source: %s , features %s, status %s, poll status %s',
                    source.bridgy_url(self), source.features, source.status,
-                   source, source.poll_status)
+                   source.poll_status)
       if source.status != 'disabled' and 'publish' in source.features:
         self.source = source
         break