d84fa75e678105194b1f7fdffe0311818cfabbf4
Ryan Barrett <git@ryanb.org>
2015-12-11 23:17:33 -0800

    update tests to match granary changes for #562

--- test/test_facebook.py
+++ test/test_facebook.py
@@ -53,14 +53,6 @@ class FacebookPageTest(testutil.ModelsTest):
                                features=['listen'])
     self.fb.put()
 
-    self.post_activity = copy.deepcopy(gr_test_facebook.SELF_ACTIVITY)
-    fb_id_and_url = {
-      'id': 'tag:facebook.com,2013:222', # this is fb_object_id
-      'url': 'https://www.facebook.com/212038/posts/222',
-      }
-    self.post_activity.update(fb_id_and_url)
-    self.post_activity['object'].update(fb_id_and_url)
-
     self.page = {
       'id': '108663232553079',
       'about': 'Our vegetarian cooking blog',
@@ -96,8 +88,9 @@ class FacebookPageTest(testutil.ModelsTest):
     owned_event = copy.deepcopy(gr_test_facebook.EVENT)
     owned_event['id'] = '888'
     owned_event['owner']['id'] = '212038'
-    self.expect_api_call('me/feed?offset=0', {'data': [gr_test_facebook.POST]})
-    self.expect_api_call('me/photos/uploaded', {'data': [gr_test_facebook.POST]})
+    self.expect_api_call('me/feed?offset=0', {'data': [
+      gr_test_facebook.POST, gr_test_facebook.PHOTO_POST]})
+    self.expect_api_call('me/photos/uploaded', {'data': [gr_test_facebook.PHOTO]})
     self.expect_api_call('me/events', {'data': [gr_test_facebook.EVENT, owned_event]})
     self.expect_api_call(gr_facebook.API_EVENT % '145304994', gr_test_facebook.EVENT)
     self.expect_api_call(gr_facebook.API_EVENT % '888', owned_event)
@@ -107,13 +100,12 @@ class FacebookPageTest(testutil.ModelsTest):
     event_activity = self.fb.gr_source.event_to_activity(owned_event)
     for k in 'attending', 'notAttending', 'maybeAttending', 'invited':
       event_activity['object'][k] = gr_test_facebook.EVENT_OBJ_WITH_ATTENDEES[k]
-    self.assert_equals([self.post_activity, gr_test_facebook.ACTIVITY, event_activity],
-                       self.fb.get_activities())
+    self.assert_equals(
+      [gr_test_facebook.ACTIVITY, gr_test_facebook.PHOTO_ACTIVITY, event_activity],
+      self.fb.get_activities())
 
   def test_get_activities_post_and_photo_duplicates(self):
-    self.assertEqual(gr_test_facebook.POST['object_id'],
-                     gr_test_facebook.PHOTO['id'])
-    self.expect_api_call('me/feed?offset=0', {'data': [gr_test_facebook.POST]})
+    self.expect_api_call('me/feed?offset=0', {'data': [gr_test_facebook.PHOTO_POST]})
     self.expect_api_call('me/photos/uploaded', {'data': [gr_test_facebook.PHOTO]})
     self.expect_api_call('me/events', {})
     self.mox.ReplayAll()
@@ -123,16 +115,18 @@ class FacebookPageTest(testutil.ModelsTest):
     obj = got[0]['object']
     self.assertEquals('tag:facebook.com,2013:222', obj['id'])
     self.assertEquals('https://www.facebook.com/212038/posts/222', obj['url'])
-    self.assertEquals(3, len(obj['replies']['items']))
-    self.assertEquals(3, len([t for t in obj['tags'] if t.get('verb') == 'like']))
+    self.assertEquals(1, len(obj['replies']['items']))
+    self.assertEquals(1, len([t for t in obj['tags'] if t.get('verb') == 'like']))
 
   def test_get_activities_canonicalizes_ids_with_colons(self):
     """https://github.com/snarfed/bridgy/issues/305"""
     # translate post id and comment ids to same ids in new colon-based format
     post = copy.deepcopy(gr_test_facebook.POST)
-    post['id'] = '212038:10100176064482163:11'
+    activity = copy.deepcopy(gr_test_facebook.ACTIVITY)
+    post['id'] = activity['fb_id'] = activity['object']['fb_id'] = \
+      '212038:10100176064482163:11'
 
-    reply = self.post_activity['object']['replies']['items'][0]
+    reply = activity['object']['replies']['items'][0]
     post['comments']['data'][0]['id'] = reply['fb_id'] = \
         '12345:547822715231468:987_6796480'
     reply['url'] = 'https://www.facebook.com/12345/posts/547822715231468?comment_id=6796480'
@@ -143,13 +137,12 @@ class FacebookPageTest(testutil.ModelsTest):
     self.expect_api_call('me/events', {})
     self.mox.ReplayAll()
 
-    self.assert_equals([self.post_activity], self.fb.get_activities())
+    self.assert_equals([activity], self.fb.get_activities())
 
   def test_get_activities_ignores_bad_comment_ids(self):
     """https://github.com/snarfed/bridgy/issues/305"""
     bad_post = copy.deepcopy(gr_test_facebook.POST)
     bad_post['id'] = '90^90'
-    del bad_post['object_id']
 
     post_with_bad_comment = copy.deepcopy(gr_test_facebook.POST)
     post_with_bad_comment['comments']['data'].append(
@@ -162,7 +155,7 @@ class FacebookPageTest(testutil.ModelsTest):
 
     # should only get the base activity, without the extra comment, and not the
     # bad activity at all
-    self.assert_equals([self.post_activity], self.fb.get_activities())
+    self.assert_equals([gr_test_facebook.ACTIVITY], self.fb.get_activities())
 
   def test_expired_sends_notification(self):
     self.expect_api_call('me/feed?offset=0',
@@ -308,9 +301,9 @@ class FacebookPageTest(testutil.ModelsTest):
     self.fb.put()
 
     # Facebook API calls
-    post = gr_test_facebook.POST
-    self.expect_api_call('me/feed?offset=0&limit=50', {'data': [post]})
-    self.expect_api_call('me/photos/uploaded', {})
+    self.expect_api_call('me/feed?offset=0&limit=50', {'data': [
+      gr_test_facebook.PHOTO_POST]})
+    self.expect_api_call('me/photos/uploaded', {'data': [gr_test_facebook.PHOTO]})
     self.expect_api_call('me/events', {})
     self.expect_api_call('sharedposts?ids=222', {})
     self.expect_api_call('comments?filter=stream&ids=222', {})
@@ -323,8 +316,8 @@ class FacebookPageTest(testutil.ModelsTest):
       </div>
     </html>""")
 
-    self.assertNotIn('222', gr_test_facebook.POST['id'])
-    self.assertEquals('222', gr_test_facebook.POST['object_id'])
+    self.assertNotIn('222', gr_test_facebook.PHOTO_POST['id'])
+    self.assertEquals('222', gr_test_facebook.PHOTO_POST['object_id'])
     self.expect_api_call('212038_222', {'id': '0', 'object_id': '222'})
     self.expect_requests_get('http://my.orig/post', """
     <html class="h-entry">
