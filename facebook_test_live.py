089b7a8c284ae653a2709d2dbcf3257dbdac2049
Ryan Barrett <git@ryanb.org>
2016-01-19 09:00:24 -0800

    update link to test user datastore entity

--- facebook_test_live.py
+++ facebook_test_live.py
@@ -8,7 +8,7 @@ https://brid.gy/facebook/1407574399567467 . To refresh his access token in
 facebook_test_user_access_token, reauth on his bridgy user page and then grab it
 from the datastore:
 
-https://appengine.google.com/datastore/edit?app_id=s~brid-gy&key=aglzfmJyaWQtZ3lyIgsSDEZhY2Vib29rQXV0aCIQMTQwNzU3NDM5OTU2NzQ2Nww
+https://console.developers.google.com/datastore/editentity?project=brid-gy&kind=FacebookPage&queryType=GQLQuery&queryText&key=0/|12/FacebookPage|21/name:1407574399567467
 
 He has one post with one like and two comments:
 https://www.facebook.com/100009447618341/posts/1407573252900915
