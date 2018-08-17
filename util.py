0024e6615253f73b2197f43a12aeaed7e62c408c
Ryan Barrett <git@ryanb.org>
2018-08-17 07:34:37 -0700

    temporarily switch poll and propagate back to the frontend service
    for #578. experiment to see how it handles the load now that facebook is gone.

--- util.py
+++ util.py
@@ -124,7 +124,7 @@ def add_poll_task(source, now=False, **kwargs):
   task = taskqueue.add(queue_name=queue,
                        params={'source_key': source.key.urlsafe(),
                                'last_polled': last_polled_str},
-                       target='background',
+                       target='default',
                        **kwargs)
   logging.info('Added %s task %s with args %s', queue, task.name, kwargs)
 
@@ -133,7 +133,7 @@ def add_propagate_task(entity, **kwargs):
   """Adds a propagate task for the given response entity."""
   task = taskqueue.add(queue_name='propagate',
                        params={'response_key': entity.key.urlsafe()},
-                       target='background',
+                       target='default',
                        **kwargs)
   logging.info('Added propagate task: %s', task.name)
 
@@ -142,7 +142,7 @@ def add_propagate_blogpost_task(entity, **kwargs):
   """Adds a propagate-blogpost task for the given response entity."""
   task = taskqueue.add(queue_name='propagate-blogpost',
                        params={'key': entity.key.urlsafe()},
-                       target='background',
+                       target='default',
                        **kwargs)
   logging.info('Added propagate-blogpost task: %s', task.name)
 
@@ -156,7 +156,7 @@ def add_discover_task(source, post_id, type=None, **kwargs):
     params['type'] = type
 
   task = taskqueue.add(queue_name='discover', params=params,
-                       target='background')
+                       target='default')
   logging.info('Added discover task for post %s for %s: %s', post_id,
                source.label(), task.name)
 
