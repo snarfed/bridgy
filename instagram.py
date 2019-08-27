a9fde11557900ff5d2b89bb4a49d03630790ced2
Ryan Barrett <git@ryanb.org>
2019-08-27 07:53:37 -0700

    instagram scraping: interpret 401 as rate limiting
    for snarfed/bridgy#665

--- instagram.py
+++ instagram.py
@@ -49,7 +49,8 @@ class Instagram(Source):
   SHORT_NAME = 'instagram'
   FAST_POLL = datetime.timedelta(minutes=120)
   RATE_LIMITED_POLL = Source.SLOW_POLL
-  RATE_HTTP_LIMIT_CODES = Source.RATE_LIMIT_HTTP_CODES + ('503',)
+  RATE_HTTP_LIMIT_CODES = ('401', '429', '503')
+  DISABLE_HTTP_CODES = ()
 
   URL_CANONICALIZER = util.UrlCanonicalizer(
     domain=GR_CLASS.DOMAIN,
