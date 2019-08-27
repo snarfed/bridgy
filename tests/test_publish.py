snarfed/bridgy

commit 358110db53b8706fafbe405e1fe03544b715be8d

publish: include original publish details in update attempt errors
for https://github.com/snarfed/bridgy/issues/84#issuecomment-520857249

publish.py
@@ -227,7 +227,8 @@ class Handler(webmention.WebmentionHandler):
     # create the Publish entity so we can store the result.
     if (self.entity.status == 'complete' and self.entity.type != 'preview' and
         not self.PREVIEW and not appengine_config.DEBUG):
-      return self.error("Sorry, you've already published that page, and Bridgy Publish doesn't yet support updating or deleting existing posts. Details: https://github.com/snarfed/bridgy/issues/84")
+      return self.error("Sorry, you've already published that page, and Bridgy Publish doesn't support updating existing posts. Details: https://github.com/snarfed/bridgy/issues/84",
+                        extra_json={'original': self.entity.published})
 
     # find rel-shortlink, if any
     # http://microformats.org/wiki/rel-shortlink

tests/test_publish.py
@@ -208,12 +208,14 @@ class PublishTest(testutil.HandlerTest):
 
     # first attempt should work
     self.assert_success('preview of foo - http://foo.com/bar', preview=True)
-    self.assert_created('foo - http://foo.com/bar')
+    created = self.assert_created('foo - http://foo.com/bar')
     self.assertEquals(5, Publish.query().count())
     self.assertEquals(3, Publish.query(Publish.status == 'complete').count())
 
     # now that there's a complete Publish entity, more attempts should fail
-    self.assert_error("Sorry, you've already published that page")
+    resp = self.assert_error("Sorry, you've already published that page")
+    self.assertEquals(json.loads(created.body), json.loads(resp.body)['original'])
+
     # try again to test for a bug we had where a second try would succeed
     self.assert_error("Sorry, you've already published that page")
     # should still be able to preview though

webmention.py
@@ -115,7 +115,7 @@ for details (skip to level 2, <em>Publishing on the IndieWeb</em>).
     return fetched, data
 
   def error(self, error, html=None, status=400, data=None, log_exception=True,
-            mail=False):
+            mail=False, extra_json=None):
     """Handle an error. May be overridden by subclasses.
 
     Args:
@@ -125,6 +125,7 @@ for details (skip to level 2, <em>Publishing on the IndieWeb</em>).
       data: mf2 data dict parsed from source page
       log_exception: boolean, whether to include a stack trace in the log msg
       mail: boolean, whether to email me
+      extra_json: dict to be merged into the JSON response body
     """
     logging.info(error, exc_info=log_exception)
 
@@ -136,6 +137,11 @@ for details (skip to level 2, <em>Publishing on the IndieWeb</em>).
     resp = {'error': error}
     if data:
       resp['parsed'] = data
+    if extra_json:
+      assert 'error' not in extra_json
+      assert 'parsed' not in extra_json
+      resp.update(extra_json)
+
     resp = json.dumps(resp, indent=2)
 
     if mail and status != 404:

commit 5d558b289ddb83850e08c9759a3b411f2f204d1a

fix interactive publish bug on already published posts
introduced by 358110db

publish.py
@@ -646,7 +646,7 @@ class PreviewHandler(Handler):
             else gr_source.INCLUDE_IF_TRUNCATED if val.lower() == 'maybe'
             else gr_source.OMIT_LINK)
 
-  def error(self, error, html=None, status=400, data=None, mail=False):
+  def error(self, error, html=None, status=400, data=None, mail=False, **kwargs):
     logging.info(error, exc_info=True)
     self.response.set_status(status)
     error = html if html else util.linkify(error)
@@ -695,7 +695,7 @@ class SendHandler(Handler):
   def include_link(self, item):
     return self.state['include_link']
 
-  def error(self, error, html=None, status=400, data=None, mail=False):
+  def error(self, error, html=None, status=400, data=None, mail=False, **kwargs):
     logging.info(error, exc_info=True)
     error = html if html else util.linkify(error)
     self.messages.add('%s' % error)

tests/test_publish.py
@@ -221,6 +221,18 @@ class PublishTest(testutil.HandlerTest):
     # should still be able to preview though
     self.assert_success('preview of foo', preview=True)
 
+  def test_already_published_interactive(self):
+    self.expect_requests_get('http://foo.com/bar', self.post_html % 'foo')
+    self.mox.ReplayAll()
+
+    page = PublishedPage(id='http://foo.com/bar')
+    Publish(parent=page.key, source=self.source.key, status='complete',
+            type='post', published={'content': 'foo'}).put()
+
+    resp = self.assert_response('', status=302, interactive=True)
+    self.assertIn("Sorry, you've already published that page",
+                  urllib.unquote_plus(resp.headers['Location']))
+
   def test_already_published_then_preview_feed_with_no_items(self):
     page = PublishedPage(id='http://foo.com/bar')
     Publish(parent=page.key, source=self.source.key, status='complete',