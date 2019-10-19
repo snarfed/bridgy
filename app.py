snarfed/bridgy

commit 4cf4da03625656452ae0d8d80ef070e399822ba6

redirect facebook and G+ user pages to docs on why they're dead

app.py
@@ -735,11 +735,24 @@ class CspReportHandler(util.Handler):
     logging.info(self.request.body)
 
 
+class FacebookIsDeadHandler(util.Handler):
+  def get(self):
+    self.redirect('/about#rip-facebook')
+
+
+class GooglePlusIsDeadHandler(util.Handler):
+  def get(self):
+    self.redirect('/about#rip-google+')
+
+
+
 application = webapp2.WSGIApplication(
   [('/?', FrontPageHandler),
    ('/users/?', UsersHandler),
-   ('/(blogger|facebook(?:-email)?|fake|fake_blog|flickr|github|googleplus|instagram|mastodon|medium|tumblr|twitter|wordpress)/([^/]+)/?',
+   ('/(blogger|facebook-email|fake|fake_blog|flickr|instagram|mastodon|medium|tumblr|twitter|wordpress)/([^/]+)/?',
     UserHandler),
+   ('/facebook/.*', FacebookIsDeadHandler),
+   ('/googleplus/.*', GooglePlusIsDeadHandler),
    ('/about/?', AboutHandler),
    ('/delete/start', DeleteStartHandler),
    ('/delete/finish', DeleteFinishHandler),

commit befe149f8cb92eed009580a27f899721585ba198

oops, i broke github user pages in 4cf4da03625656452ae0d8d80ef070e399822ba6. fix that.
thanks for reporting @johanbove!

app.py
@@ -749,7 +749,7 @@ class GooglePlusIsDeadHandler(util.Handler):
 application = webapp2.WSGIApplication(
   [('/?', FrontPageHandler),
    ('/users/?', UsersHandler),
-   ('/(blogger|facebook-email|fake|fake_blog|flickr|instagram|mastodon|medium|tumblr|twitter|wordpress)/([^/]+)/?',
+   ('/(blogger|facebook-email|fake|fake_blog|flickr|github|instagram|mastodon|medium|tumblr|twitter|wordpress)/([^/]+)/?',
     UserHandler),
    ('/facebook/.*', FacebookIsDeadHandler),
    ('/googleplus/.*', GooglePlusIsDeadHandler),