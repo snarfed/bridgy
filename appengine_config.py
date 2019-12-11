4fdc56fee42fa4e9b943753f51f15b9cbf80ac6c
Ryan Barrett <git@ryanb.org>
2019-12-11 07:56:48 -0800

    python 3: update o-d static file handlers in app.yaml
    ...via symlink so that gcloud app deploy uploads them. right now they're ignored because local3/ is in .gcloudignore. i tried to tell gcloud to explicitly include them with this line, and similar variations:
    
    !local3/lib/python3.6/site-packages/oauth_dropins/static/
    
    based on https://cloud.google.com/sdk/gcloud/reference/topic/gcloudignore#SYNTAX , but no luck.

--- appengine_config.py
+++ appengine_config.py
@@ -1,11 +1,12 @@
 """Bridgy App Engine config.
 """
-import os
-os.environ.setdefault('CLOUDSDK_CORE_PROJECT', 'brid-gy')
-os.environ.setdefault('DATASTORE_DATASET', 'brid-gy')
-os.environ.setdefault('GOOGLE_CLOUD_PROJECT', 'brid-gy')
-
 from granary.appengine_config import *
+
+import os
+if DEBUG:
+  os.environ.setdefault('CLOUDSDK_CORE_PROJECT', 'brid-gy')
+  os.environ.setdefault('DATASTORE_DATASET', 'brid-gy')
+  os.environ.setdefault('GOOGLE_CLOUD_PROJECT', 'brid-gy')
 
 DISQUS_ACCESS_TOKEN = read('disqus_access_token')
 DISQUS_API_KEY = read('disqus_api_key')
