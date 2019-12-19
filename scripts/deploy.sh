dee7ca39acd79e6b971ab1ba2536175c498bec71
Ryan Barrett <git@ryanb.org>
2019-12-19 13:54:52 -0800

    python 3: update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,42 +3,40 @@
 # Checks pre-deploy safeguards - tests, app keys, package versions - then deploys.
 #
 # Expects that your local bridgy, granary, and oauth-dropins repos are all in
-# the same directory, and that you have the App Engine SDK installed.
-#
-# TODO: check that granary, oauth-dropins, and webutil are up to date.
+# the same directory, and that you have gcloud installed.
 
 set -e
 src=`dirname $0`/../..
 
 # run unit tests
-cd $src/oauth-dropins && source local/bin/activate
+pkill datastore || true
+gcloud beta emulators datastore start --no-store-on-disk --consistency=1.0 --host-port=localhost:8089 < /dev/null >& /dev/null &
+sleep 2s
+
+cd $src/oauth-dropins && source local3/bin/activate
 python -m unittest discover --pattern="test_*.py"
 
-cd ../granary && source local/bin/activate
+cd ../granary && source local3/bin/activate
 python -m unittest discover
 
-cd ../bridgy && source local/bin/activate
-python -m unittest discover
+cd ../bridgy && source local3/bin/activate
+python -m unittest discover -s tests -t .
+
+kill %1  # datastore emulator
 
 # check silo app keys (aka client ids)
 md5sum -c keys.md5
 
-# check package versions
-missing=`pip freeze -q -r requirements.freeze.txt | join --nocheck-order -v 2 - requirements.freeze.txt`
-if [[ "$missing" != "" ]]; then
-  echo 'ERROR: Package version mismatch! Expected:'
-  echo $missing
-  exit 1
-fi
+# # TODO: check package versions
+# missing=`pip freeze -q -r requirements.freeze.txt | join --nocheck-order -v 2 - requirements.freeze.txt`
+# if [[ "$missing" != "" ]]; then
+#   echo 'ERROR: Package version mismatch! Expected:'
+#   echo $missing
+#   exit 1
+# fi
 
-echo 'Package versions OK.'
+# echo 'Package versions OK.'
 
-# push commits and deploy!
-# git push
-# set an explicit version since logs.py's log fetching depends on explicitly
-# enumerating all versions. otherwise gcloud generates a new timestamp-based
-# version by default for every deploy.
-#
-# https://cloud.google.com/appengine/docs/standard/python/tools/uploadinganapp#deploying_an_app
+# deploy!
 # https://cloud.google.com/sdk/gcloud/reference/app/deploy
-gcloud -q app deploy --account heaven@gmail.com --project brid-gy --version 8 app.yaml
+gcloud -q app deploy --project brid-gy *.yaml
