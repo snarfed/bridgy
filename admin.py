snarfed/bridgy

commit 5f1b9cbedaa87d8ea26082369cb4367b92318b18

include logs module in jinja templates manually
...since i had to drop the global include in snarfed/webutil@0be2d832b394c4d7bbf89873e85cae33906b9352 to avoid a circular import.

admin.py
@@ -11,6 +11,7 @@ from models import BlogPost, Response, Source
 import util
 
 from google.cloud import ndb
+from oauth_dropins.webutil import logs
 from oauth_dropins.webutil.util import json_dumps, json_loads
 import webapp2
 
@@ -47,7 +48,7 @@ class ResponsesHandler(handlers.TemplateHandler):
         entities.append(e)
 
     entities.sort(key=lambda e: (e.source, e.activities, e.response))
-    return {'responses': entities}
+    return {'responses': entities, 'logs': logs}
 
 
 class SourcesHandler(handlers.TemplateHandler):

app.py
@@ -13,7 +13,7 @@ from oauth_dropins import indieauth
 from oauth_dropins.webutil import appengine_info
 from oauth_dropins.webutil import handlers as webutil_handlers
 from oauth_dropins.webutil.appengine_config import ndb_client
-from oauth_dropins.webutil.logs import LogHandler
+from oauth_dropins.webutil import logs
 from oauth_dropins.webutil.util import json_dumps, json_loads
 import webapp2
 
@@ -228,6 +228,7 @@ class UserHandler(DashboardHandler):
     vars.update({
       'source': self.source,
       'sources': models.sources,
+      'logs': logs,
       'EPOCH': util.EPOCH,
       'REFETCH_HFEED_TRIGGER': models.REFETCH_HFEED_TRIGGER,
       'RECENT_PRIVATE_POSTS_THRESHOLD': RECENT_PRIVATE_POSTS_THRESHOLD,
@@ -746,7 +747,7 @@ routes += [
   ('/edit-websites', EditWebsites),
   ('/logout', LogoutHandler),
   ('/csp-report', CspReportHandler),
-  ('/log', LogHandler),
+  ('/log', logs.LogHandler),
   ('/_ah/(start|stop|warmup)', util.NoopHandler),
 ]
 

commit 5ba7dd9398ae8162e6038ff96b1d67a43d85893c

fix /admin/sources
broke in 5f1b9cbedaa87d8ea26082369cb4367b92318b18 due to requiring `logs` in template vars specifically.

fixes https://console.cloud.google.com/errors/CPjE1Ofhh_PweQ

admin.py
@@ -68,6 +68,7 @@ class SourcesHandler(handlers.TemplateHandler):
                for cls in CLASSES]
     return {
       'sources': itertools.chain(*[q.get_result() for q in queries]),
+      'logs': logs,
     }
 
 