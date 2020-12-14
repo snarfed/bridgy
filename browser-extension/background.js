4256beea75dd24211f8252138d798674e23a081b
Ryan Barrett <git@ryanb.org>
2020-12-14 12:09:17 -0800

    browser extension background script hello world. works!

---
+++ browser-extension/background.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/**
+ * Makes an HTTP GET request and returns the JSON response.
+ */
+async function fetchJson() {
+  const url = 'https://granary.io/instagram/snarfed/@self/@app/?format=mf2-json&cookie=...&interactive=true'
+  console.log(`Fetching ${url}`)
+  const res = await fetch(url, {method: 'GET'})
+  if (!res.ok) {
+    console.log(`Granary error: ${res} ${res.status} ${res.statusText}`)
+    return null
+  }
+  return await res.json()
+}
+
+console.log('Starting')
+fetchJson().then((res) => {
+  console.log(`Got ${res.items.length} items`)
+  console.log(`${res.items[0].properties.content[0]}`)
+})
