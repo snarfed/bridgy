e4ae544f2023408a16fecc27f167985435952a9d
Ryan Barrett <git@ryanb.org>
2020-12-22 14:33:18 -0800

    browser extension: add recurring alarm to poll every 30m

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,4 +1,21 @@
 import {poll} from './instagram.js'
 
-console.log('Starting poll...')
-poll().then(() => console.log('Done!'))
+const FREQUENCY_MIN = 30
+
+function doPoll(alarm) {
+  console.log('Starting poll...')
+  poll().then(() => console.log('Done!'))
+}
+
+console.log(`Scheduling poll every ${FREQUENCY_MIN}m`)
+browser.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name == 'bridgy-instagram-poll') {
+    doPoll()
+  }
+})
+browser.alarms.create('bridgy-instagram-poll', {
+  delayInMinutes: 0,
+  periodInMinutes: FREQUENCY_MIN,
+})
+
+doPoll()
