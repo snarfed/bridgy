85cd293beaf43f395de6b317cdd7f2b15a4a77aa
Ryan Barrett <git@ryanb.org>
2021-01-28 16:53:43 -0800

    refactor instagram.py to use browser.py

--- instagram.py
+++ instagram.py
@@ -1,26 +1,13 @@
-"""Instagram code and datastore model classes.
+"""Instagram browser extension source class and request handlers.
 """
-from datetime import timedelta
-import logging
-
-from google.cloud import ndb
 from granary import instagram as gr_instagram
-from granary import microformats2
-from granary import source as gr_source
-from oauth_dropins.webutil.util import json_dumps, json_loads
-import webapp2
+from oauth_dropins import instagram as oauth_instagram
 
-from oauth_dropins import indieauth
-from oauth_dropins import instagram as od_instagram
-
-from models import Activity, Domain, Source, MAX_AUTHOR_URLS
+import browser
 import util
 
-JSON_CONTENT_TYPE = 'application/json'
 
-
-class Instagram(Source):
-
+class Instagram(browser.BrowserSource):
   """An Instagram account.
 
   The key name is the username. Instagram usernames may have ASCII letters (case
@@ -29,11 +16,7 @@ class Instagram(Source):
   """
   GR_CLASS = gr_instagram.Instagram
   SHORT_NAME = 'instagram'
-  CAN_LISTEN = True
-  CAN_PUBLISH = False
-  AUTO_POLL = False
-  SLOW_POLL = FAST_POLL = timedelta(0)
-  OAUTH_START_HANDLER = None
+  OAUTH_START_HANDLER = oauth_instagram.StartHandler
   URL_CANONICALIZER = util.UrlCanonicalizer(
     domain=GR_CLASS.DOMAIN,
     subdomain='www',
@@ -45,28 +28,6 @@ class Instagram(Source):
   # blank granary Instagram object, shared across all instances
   gr_source = gr_instagram.Instagram()
 
-  @staticmethod
-  def new(handler, auth_entity=None, actor=None, **kwargs):
-    """Creates and returns an :class:`Instagram` for the logged in user.
-
-    Args:
-      handler: the current :class:`webapp2.RequestHandler`
-    """
-    assert not auth_entity
-    assert actor
-
-    username = actor['username']
-    if not kwargs.get('features'):
-      kwargs['features'] = ['listen']
-
-    ig = Instagram(id=username,
-                   name=actor.get('displayName'),
-                   picture=actor.get('image', {}).get('url'),
-                   url=gr_instagram.Instagram.user_url(username),
-                   **kwargs)
-    ig.domain_urls, ig.domains = ig._urls_and_domains(None, None, actor=actor)
-    return ig
-
   def silo_url(self):
     """Returns the Instagram account URL, e.g. https://instagram.com/foo."""
     return self.url
@@ -75,249 +36,5 @@ class Instagram(Source):
     """Returns the username."""
     return self.key_id()
 
-  @classmethod
-  def button_html(cls, feature, **kwargs):
-    return od_instagram.StartHandler.button_html(
-      '/about#browser-extension',
-      form_method='get',
-      image_prefix='/oauth_dropins/static/')
 
-  def get_activities_response(self, *args, **kwargs):
-    """Uses Activity entities stored in the datastore."""
-    activities = []
-
-    activity_id = kwargs.get('activity_id')
-    if activity_id:
-      activity = Activity.get_by_id(self.gr_source.tag_uri(activity_id))
-      if activity:
-        activities = [activity]
-    else:
-      activities = Activity.query(Activity.source == self.key)\
-                           .order(-Activity.updated).fetch(50)
-
-    return self.gr_source.make_activities_base_response(
-      [json_loads(a.activity_json) for a in activities])
-
-  def get_comment(self, comment_id,  activity=None, **kwargs):
-    """Uses the activity passed in the activity kwarg."""
-    if activity:
-      for reply in activity.get('object', {}).get('replies', {}).get('items', []):
-        parsed = util.parse_tag_uri(reply.get('id', ''))
-        if parsed and parsed[1] == comment_id:
-          return reply
-
-  def get_like(self, activity_user_id, activity_id, like_user_id, activity=None,
-               **kwargs):
-    """Uses the activity passed in the activity kwarg."""
-    if activity:
-      for tag in activity.get('object', {}).get('tags', []):
-        if tag.get('verb') == 'like':
-          parsed = util.parse_tag_uri(tag.get('author', {}).get('id', ''))
-          if parsed and parsed[1] == like_user_id:
-            return tag
-
-
-class BrowserHandler(util.Handler):
-  """Base class for requests from the browser extension."""
-  ig = gr_instagram.Instagram()
-
-  def options(self):
-    self.response.headers['Access-Control-Allow-Origin'] = '*'
-    self.response.headers['Access-Control-Allow-Methods'] = '*'
-
-  def output(self, obj):
-    self.response.headers['Content-Type'] = JSON_CONTENT_TYPE
-    self.response.write(json_dumps(obj, indent=2))
-
-  def parse_activities(self):
-    """Parses Instagram HTML (in the POST body) into logged in user and posts.
-
-    Returns:
-      (list of dicts, dict): AS1 activities and logged in actor
-    """
-    # parse the Instagram profile HTML
-    activities, actor = self.ig.html_to_activities(self.request.text)
-    if not actor or not actor.get('username'):
-      self.abort(400, "Couldn't determine logged in Instagram user")
-
-    # check that the instagram account is public
-    if not gr_source.Source.is_public(actor):
-      self.abort(400, 'Your Instagram account is private. Bridgy only supports public accounts.')
-
-    return activities, actor
-
-  def check_token(self, actor):
-    """If the request's token is not stored for the actor's domain, returns 400.
-
-    Args:
-      actor: dict, AS1 actor for logged in Instagram user
-    """
-    token = util.get_required_param(self, 'token')
-
-    domains = set(util.domain_from_link(u) for u in microformats2.object_urls(actor))
-    domains.discard(gr_instagram.Instagram.DOMAIN)
-    for domain in ndb.get_multi(ndb.Key(Domain, d) for d in domains):
-      if domain and token in domain.tokens:
-        return
-
-    self.abort(400, f'Token {token} is not authorized for any of: {domains}')
-
-
-class HomepageHandler(BrowserHandler):
-  """Parses an Instagram home page and returns the logged in user's username.
-
-  Request body is https://www.instagram.com/ HTML for a logged in user.
-  """
-  def post(self):
-    _, actor = self.parse_activities()
-    logging.info(f"Returning {actor['username']}")
-    self.output(actor['username'])
-
-
-class ProfileHandler(BrowserHandler):
-  """Parses an Instagram profile page and returns the posts' URLs.
-
-  Request body is HTML from an IG profile, eg https://www.instagram.com/name/ ,
-  for a logged in user.
-
-  Response body is the JSON list of translated ActivityStreams activities.
-  """
-  def post(self):
-    activities, actor = self.parse_activities()
-    self.check_token(actor)
-
-    # create/update the Bridgy account
-    Instagram.create_new(self, actor=actor)
-
-    ids = ' '.join(a['id'] for a in activities)
-    logging.info(f"Returning activities for {actor['username']}: {ids}")
-    self.output(activities)
-
-
-class PostHandler(BrowserHandler):
-  """Parses an Instagram post and creates or updates a stored Activity.
-
-  Request body is HTML from an IG photo/video permalink, eg
-  https://www.instagram.com/p/ABC123/ , for a logged in user.
-
-  Response body is the translated ActivityStreams activity JSON.
-  """
-  @ndb.transactional()
-  def post(self):
-    activities, actor = self.parse_activities()
-    self.check_token(actor)
-
-    if len(activities) != 1:
-      self.abort(400, f'Expected 1 Instagram post, got {len(activities)}')
-    new_activity = activities[0]
-    id = new_activity.get('id')
-    if not id:
-      self.abort(400, 'Instagram post missing id')
-
-    username = new_activity['object']['author']['username']
-    source = Instagram.get_by_id(username)
-    if not source:
-      self.abort(404, f'No account found for Instagram user {username}')
-
-    activity = Activity.get_by_id(id)
-    if activity:
-      # we already have this activity! merge in the existing comments.
-      existing = json_loads(activity.activity_json)
-      replies = existing['object'].setdefault('replies', {})
-      new_replies = new_activity['object'].get('replies', {}).get('items', [])
-      gr_source.merge_by_id(replies, 'items', new_replies)
-      replies['totalItems'] = len(replies['items'])
-      activity.activity_json = json_dumps(existing)
-      activity.put()
-    else:
-      # store the new activity
-      Activity(id=id, source=source.key, activity_json=json_dumps(new_activity)).put()
-
-    logging.info(f"Stored activity {id}")
-    self.output(new_activity)
-
-
-class LikesHandler(BrowserHandler):
-  """Parses a list of Instagram likes and adds them to an existing Activity.
-
-  Requires the request parameter `id` with the IG post's id (not shortcode!).
-
-  Request body is a JSON list of IG likes for a post that's already been created
-  via the /instagram/browser/post endpoint.
-
-  Response body is the translated ActivityStreams JSON for the likes.
-  """
-  def post(self):
-    id = util.get_required_param(self, 'id')
-
-    # validate request
-    parsed_id = util.parse_tag_uri(id)
-    if not parsed_id:
-      self.abort(400, f'Expected id to be tag URI; got {id}')
-
-    activity = Activity.get_by_id(id)
-    if not activity:
-      self.abort(404, f'No Instagram post found for id {id}')
-
-    activity_data = json_loads(activity.activity_json)
-    obj = activity_data['object']
-    actor = obj.get('author') or activity.get('actor')
-    self.check_token(actor=actor)
-
-    # convert new likes to AS
-    container = {
-      'id': parsed_id[1],
-      'edge_media_preview_like': {
-        # corresponds to same code in gr_instagram.Instagram.html_to_activities()
-        'edges': self.request.json.get('data', {}).get('shortcode_media', {})\
-                                  .get('edge_liked_by', {}).get('edges', [])
-      },
-    }
-    new_likes = self.ig._json_media_node_to_activity(container)['object']['tags']
-
-    # merge them into existing activity
-    gr_source.merge_by_id(obj, 'tags', new_likes)
-    activity.activity_json = json_dumps(activity_data)
-    activity.put()
-
-    like_ids = ' '.join(l['id'] for l in new_likes)
-    logging.info(f"Stored likes for activity {id}: {like_ids}")
-    self.output(new_likes)
-
-
-class PollHandler(BrowserHandler):
-  """Triggers a poll for an Instagram account.
-
-  Requires the `username` parameter.
-  """
-  def post(self):
-    username = util.get_required_param(self, 'username')
-    source = Instagram.get_by_id(username)
-    if not source:
-      self.abort(404, f'No account found for Instagram user {username}')
-
-    util.add_poll_task(source)
-
-    self.output('OK')
-
-
-class TokenDomainsHandler(BrowserHandler):
-  """Returns the domains that a token is registered for."""
-  def post(self):
-    token = util.get_required_param(self, 'token')
-
-    domains = [d.key.id() for d in Domain.query(Domain.tokens == token)]
-    if not domains:
-      self.abort(404, f'No registered domains for token {token}')
-
-    self.output(domains)
-
-
-ROUTES = [
-  ('/instagram/browser/homepage', HomepageHandler),
-  ('/instagram/browser/profile', ProfileHandler),
-  ('/instagram/browser/post', PostHandler),
-  ('/instagram/browser/likes', LikesHandler),
-  ('/instagram/browser/poll', PollHandler),
-  ('/instagram/browser/token-domains', TokenDomainsHandler),
-]
+ROUTES = browser.routes(Instagram)
--- tests/test_instagram.py
+++ tests/test_instagram.py
@@ -8,15 +8,12 @@ import appengine_config  # injects 2013 into tag URIs in test_instagram objects
 
 from granary import instagram as gr_instagram
 from granary.tests.test_instagram import (
-  COMMENT_OBJS,
   HTML_FEED_COMPLETE,
   HTML_FOOTER,
   HTML_HEADER,
-  HTML_PHOTO,
   HTML_PHOTO_ACTIVITY,
   HTML_PHOTO_ACTIVITY_LIKES,
   HTML_PHOTO_LIKES_RESPONSE,
-  HTML_PROFILE,
   HTML_PROFILE_COMPLETE,
   HTML_PROFILE_PRIVATE_COMPLETE,
   HTML_VIDEO_ACTIVITY,
@@ -26,19 +23,13 @@ from granary.tests.test_instagram import (
   HTML_VIEWER_CONFIG,
   LIKE_OBJS,
 )
-from oauth_dropins.webutil.testutil import TestCase
 from oauth_dropins.webutil.util import json_dumps, json_loads
 import requests
 
 import app
 from instagram import Instagram
 from models import Activity, Domain
-from .testutil import ModelsTest, instagram_profile_user
-import util
-
-PROFILE_USER = copy.deepcopy(
-  HTML_PROFILE['entry_data']['ProfilePage'][0]['graphql']['user'])
-PROFILE_USER['id'] = '987'
+from .testutil import ModelsTest
 
 HTML_VIDEO_WITH_VIEWER = copy.deepcopy(HTML_VIDEO_PAGE)
 HTML_VIDEO_WITH_VIEWER['config'] = HTML_VIEWER_CONFIG
@@ -50,7 +41,7 @@ class InstagramTest(ModelsTest):
   def setUp(self):
     super(InstagramTest, self).setUp()
     self.handler.messages = []
-    self.inst = Instagram.new(
+    self.ig = Instagram.new(
       self.handler, actor={
         'objectType': 'person',
         'id': 'tag:instagram.com,2013:420973239',
@@ -74,13 +65,13 @@ class InstagramTest(ModelsTest):
     return Activity(id='tag:instagram.com,2013:123_456', activity_json=activity_json).put()
 
   def test_new(self):
-    self.assertIsNone(self.inst.auth_entity)
-    self.assertEqual('snarfed', self.inst.key.string_id())
-    self.assertEqual('http://pic.ture/url', self.inst.picture)
-    self.assertEqual('https://www.instagram.com/snarfed/', self.inst.url)
-    self.assertEqual('https://www.instagram.com/snarfed/', self.inst.silo_url())
-    self.assertEqual('Ryan Barrett', self.inst.name)
-    self.assertEqual('snarfed (Instagram)', self.inst.label())
+    self.assertIsNone(self.ig.auth_entity)
+    self.assertEqual('snarfed', self.ig.key.string_id())
+    self.assertEqual('http://pic.ture/url', self.ig.picture)
+    self.assertEqual('https://www.instagram.com/snarfed/', self.ig.url)
+    self.assertEqual('https://www.instagram.com/snarfed/', self.ig.silo_url())
+    self.assertEqual('Ryan Barrett', self.ig.name)
+    self.assertEqual('snarfed (Instagram)', self.ig.label())
 
   def test_canonicalize_url(self):
     self.unstub_requests_head()
@@ -91,28 +82,28 @@ class InstagramTest(ModelsTest):
         'https://instagram.com/p/abcd',
     ):
       self.assertEqual('https://www.instagram.com/p/abcd/',
-                       self.inst.canonicalize_url(url))
+                       self.ig.canonicalize_url(url))
 
-    self.assertIsNone(self.inst.canonicalize_url('https://www.foo.com/p/abcd/'))
+    self.assertIsNone(self.ig.canonicalize_url('https://www.foo.com/p/abcd/'))
 
   def test_canonicalize_url_approve_checks_full_url(self):
     """...specifically, that the regex ends with a $
     https://github.com/snarfed/bridgy/issues/686
     """
     self.assertEqual('https://www.instagram.com/p/abcd/123/',
-                     self.inst.canonicalize_url('https://www.instagram.com/p/abcd/123'))
+                     self.ig.canonicalize_url('https://www.instagram.com/p/abcd/123'))
 
   def test_get_activities_response_activity_id(self):
     Activity(id='tag:instagram.com,2013:123',
              activity_json=json_dumps({'foo': 'bar'})).put()
 
-    resp = self.inst.get_activities_response(activity_id='123')
+    resp = self.ig.get_activities_response(activity_id='123')
     self.assertEqual([{'foo': 'bar'}], resp['items'])
 
   def test_get_activities_response_no_activity_id(self):
-    Activity(id='tag:instagram.com,2013:123', source=self.inst.key,
+    Activity(id='tag:instagram.com,2013:123', source=self.ig.key,
              activity_json=json_dumps({'foo': 'bar'})).put()
-    Activity(id='tag:instagram.com,2013:456', source=self.inst.key,
+    Activity(id='tag:instagram.com,2013:456', source=self.ig.key,
              activity_json=json_dumps({'baz': 'biff'})).put()
 
     other = Instagram.new(self.handler, actor={'username': 'other'}).put()
@@ -120,34 +111,34 @@ class InstagramTest(ModelsTest):
              activity_json=json_dumps({'boo': 'bah'})).put()
 
 
-    resp = self.inst.get_activities_response()
+    resp = self.ig.get_activities_response()
     self.assert_equals([{'foo': 'bar'}, {'baz': 'biff'}], resp['items'])
 
   def test_get_activities_response_no_stored_activity(self):
-    resp = self.inst.get_activities_response(activity_id='123')
+    resp = self.ig.get_activities_response(activity_id='123')
     self.assertEqual([], resp['items'])
 
   def test_get_comment(self):
     self.assert_equals(
       HTML_VIDEO_EXTRA_COMMENT_OBJ,
-      self.inst.get_comment('020', activity=HTML_VIDEO_ACTIVITY_FULL))
+      self.ig.get_comment('020', activity=HTML_VIDEO_ACTIVITY_FULL))
 
   def test_get_comment_no_matching_id(self):
-    self.assertIsNone(self.inst.get_comment('333', activity=HTML_VIDEO_ACTIVITY_FULL))
+    self.assertIsNone(self.ig.get_comment('333', activity=HTML_VIDEO_ACTIVITY_FULL))
 
   def test_get_comment_no_activity_kwarg(self):
-    self.assertIsNone(self.inst.get_comment('020'))
+    self.assertIsNone(self.ig.get_comment('020'))
 
   def test_get_like(self):
-    self.assert_equals(LIKE_OBJS[1], self.inst.get_like(
+    self.assert_equals(LIKE_OBJS[1], self.ig.get_like(
       'unused', '123', '9', activity=HTML_PHOTO_ACTIVITY_LIKES))
 
   def test_get_like_no_matching_user(self):
-    self.assertIsNone(self.inst.get_like(
+    self.assertIsNone(self.ig.get_like(
       'unused', '123', '222', activity=HTML_PHOTO_ACTIVITY_LIKES))
 
   def test_get_like_no_activity_kwarg(self):
-    self.assertIsNone(self.inst.get_like('unused', '123', '9'))
+    self.assertIsNone(self.ig.get_like('unused', '123', '9'))
 
   def test_homepage(self):
     resp = app.application.get_response(
@@ -212,7 +203,7 @@ class InstagramTest(ModelsTest):
     self.assertIn('', resp.text)
 
   def test_post(self):
-    source = Instagram.create_new(self.handler, actor={'username': 'jc'})
+    self.ig.put()
 
     resp = app.application.get_response(
       '/instagram/browser/post?token=towkin', method='POST',
@@ -222,7 +213,7 @@ class InstagramTest(ModelsTest):
 
     activities = Activity.query().fetch()
     self.assertEqual(1, len(activities))
-    self.assertEqual(source.key, activities[0].source)
+    self.assertEqual(self.ig.key, activities[0].source)
     self.assertEqual(HTML_VIDEO_ACTIVITY_FULL, json_loads(activities[0].activity_json))
 
   def test_post_no_source(self):
@@ -230,30 +221,30 @@ class InstagramTest(ModelsTest):
       '/instagram/browser/post?token=towkin', method='POST',
       text=HTML_VIDEO_COMPLETE)
     self.assertEqual(404, resp.status_int)
-    self.assertIn('No account found for Instagram user jc', resp.text)
+    self.assertIn('No account found for Instagram user snarfed', resp.text)
 
   def test_post_empty(self):
     empty = HTML_HEADER + json_dumps({'config': HTML_VIEWER_CONFIG}) + HTML_FOOTER
     resp = app.application.get_response(
       '/instagram/browser/post?token=towkin', method='POST', text=empty)
     self.assertEqual(400, resp.status_int)
-    self.assertIn('Expected 1 Instagram post', resp.text)
+    self.assertIn('No Instagram post found in HTML', resp.text)
 
   def test_post_missing_token(self):
     empty = HTML_HEADER + json_dumps({'config': HTML_VIEWER_CONFIG}) + HTML_FOOTER
     resp = app.application.get_response(
-      '/instagram/browser/post', method='POST', text=empty)
+      '/instagram/browser/post', method='POST', text=HTML_VIDEO_COMPLETE)
     self.assertEqual(400, resp.status_int)
     self.assertIn('Missing required parameter: token', resp.text)
 
   def test_post_merge_comments(self):
-    source = Instagram.create_new(self.handler, actor={'username': 'jc'})
+    self.ig.put()
 
     # existing activity with one of the two comments in HTML_VIDEO_COMPLETE
     existing_activity = copy.deepcopy(HTML_VIDEO_ACTIVITY)
     existing_activity['object']['replies'] = {
       'totalItems': 1,
-      'items': [COMMENT_OBJS[0]],
+      'items': [HTML_VIDEO_ACTIVITY_FULL['object']['replies']['items'][0]],
     }
     activity_key = Activity(id='tag:instagram.com,2013:789_456',
                             activity_json=json_dumps(existing_activity)).put()
@@ -276,16 +267,12 @@ class InstagramTest(ModelsTest):
     resp = app.application.get_response(
       '/instagram/browser/likes?id=tag:instagram.com,2013:123_456&token=towkin',
       method='POST', text=json_dumps(HTML_PHOTO_LIKES_RESPONSE))
-    self.assertEqual(200, resp.status_int, resp.text)
 
-    expected_likes = copy.deepcopy(LIKE_OBJS)
-    for like in expected_likes:
-      del like['object']
-      del like['url']
-    self.assertEqual(expected_likes, resp.json)
+    self.assertEqual(200, resp.status_int, resp.text)
+    self.assertEqual(LIKE_OBJS, resp.json)
 
     activity = json_loads(key.get().activity_json)
-    self.assertEqual(expected_likes, activity['object']['tags'])
+    self.assertEqual(LIKE_OBJS, activity['object']['tags'])
 
   def test_likes_bad_id(self):
     resp = app.application.get_response(
@@ -311,10 +298,11 @@ class InstagramTest(ModelsTest):
     self.assertIn("nope is not authorized for any of: {'snarfed.org'}", resp.text)
 
   def test_poll(self):
-    source = Instagram.create_new(self.handler, actor={'username': 'snarfed'})
-    self.expect_task('poll', eta_seconds=0, source_key=source.key,
+    self.ig.put()
+    self.expect_task('poll', eta_seconds=0, source_key=self.ig.key,
                      last_polled='1970-01-01-00-00-00')
     self.mox.ReplayAll()
+
     resp = app.application.get_response(
       '/instagram/browser/poll?username=snarfed', method='POST')
     self.assertEqual(200, resp.status_int, resp.text)
