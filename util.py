8fa41f61a1961f1db4963750c59f762a8e8f8c12
Ryan Barrett <git@ryanb.org>
2021-03-25 16:18:00 -0700

    target tasks at background service via queue config instead of code
    propagate tasks started going to default during a cloud task outage, https://status.cloud.google.com/incident/cloud-tasks/21001, even though they were aimed at background via app_engine_routing in the cloud tasks API. switching to this instead.
    
    https://cloud.google.com/appengine/docs/standard/python/config/queueref#target
    
    also see 80ee2dddae60b00f59b3aa4fee79403457371e36

--- util.py
+++ util.py
@@ -161,7 +161,6 @@ def add_task(queue, eta_seconds=None, **kwargs):
     'app_engine_http_request': {
       'http_method': 'POST',
       'relative_uri': '/_ah/queue/%s' % queue,
-      'app_engine_routing': {'service': 'background'},
       'body': urllib.parse.urlencode(util.trim_nulls(kwargs)).encode(),
       # https://googleapis.dev/python/cloudtasks/latest/gapic/v2/types.html#google.cloud.tasks_v2.types.AppEngineHttpRequest.headers
       'headers': {'Content-Type': 'application/x-www-form-urlencoded'},
