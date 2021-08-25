f51c91d5e46c178e55c530b89ca386d436d89d9b
Ryan Barrett <git@ryanb.org>
2021-08-25 15:40:58 -0700

    Flask port: use canned secret key in dev and tests

--- config.py
+++ config.py
@@ -4,12 +4,13 @@ https://flask.palletsprojects.com/en/latest/config/
 """
 from oauth_dropins.webutil import appengine_info, util
 
-SECRET_KEY = util.read('flask_secret_key')
 JSONIFY_PRETTYPRINT_REGULAR = True
 
 if appengine_info.DEBUG:
   ENV = 'development'
   CACHE_TYPE = 'NullCache'
+  SECRET_KEY = 'sooper seekret'
 else:
   ENV = 'production'
   CACHE_TYPE = 'SimpleCache'
+  SECRET_KEY = util.read('flask_secret_key')
