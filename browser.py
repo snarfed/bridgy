snarfed/bridgy

commit 323b2b52ef857ea2ab86d3be9e4166c1dee597e8

browser extension endpoints: follow redirects, check blocklist etc for profile links

browser.py
@@ -85,7 +85,7 @@ class BrowserSource(Source):
               name=actor.get('displayName'),
               picture=actor.get('image', {}).get('url'),
               **kwargs)
-    src.domain_urls, src.domains = src._urls_and_domains(None, None, actor=actor)
+    src.domain_urls, src.domains = src.urls_and_domains(None, None, actor=actor)
     return src
 
   @classmethod
@@ -144,6 +144,9 @@ class BrowserView(View):
   def check_token_for_actor(self, actor):
     """Checks that the given actor is public and matches the request's token.
 
+    Returns: actor, with 'url' field removed and 'urls' set to only the resolved,
+      non-blocklisted urls
+
     Raises: :class:`HTTPException` with HTTP 403
     """
     if not actor:
@@ -153,14 +156,16 @@ class BrowserView(View):
       self.error(f'Your {self.gr_source().NAME} account is private. Bridgy only supports public accounts.')
 
     token = request.values['token']
-    domains = {util.domain_from_link(util.replace_test_domains_with_localhost(u))
-               for u in microformats2.object_urls(actor)}
-    domains.discard(self.source_class().GR_CLASS.DOMAIN)
+    urls, domains = self.source_class().urls_and_domains(None, None, actor=actor)
+
+    # update actor so resolved URLs can be reused
+    actor.pop('url', None)
+    actor['urls'] = [{'value': url} for url in urls]
 
     logging.info(f'Checking token against domains {domains}')
     for domain in ndb.get_multi(ndb.Key(Domain, d) for d in domains):
       if domain and token in domain.tokens:
-        return
+        return actor
 
     self.error(f'Token {token} is not authorized for any of: {domains}', 403)
 
@@ -263,7 +268,8 @@ class Profile(Feed):
     _, actor = self.scrape()
     if not actor:
       actor = self.gr_source().scraped_to_actor(request.get_data(as_text=True))
-    self.check_token_for_actor(actor)
+    # updated actor here has only non-silo, non-blocklisted 'urls' field
+    actor = self.check_token_for_actor(actor)
 
     # create/update the Bridgy account
     source = self.source_class().create_new(self, actor=actor)

models.py
@@ -519,7 +519,7 @@ class Source(StringIdModel, metaclass=SourceMeta):
     if not source.domain_urls:  # defer to the source if it already set this
       auth_entity = kwargs.get('auth_entity')
       if auth_entity and hasattr(auth_entity, 'user_json'):
-        source.domain_urls, source.domains = source._urls_and_domains(
+        source.domain_urls, source.domains = source.urls_and_domains(
           auth_entity, user_url)
     logging.debug(f'URLs/domains: {source.domain_urls} {source.domains}')
 
@@ -609,7 +609,8 @@ class Source(StringIdModel, metaclass=SourceMeta):
 
     self.put()
 
-  def _urls_and_domains(self, auth_entity, user_url, actor=None):
+  @classmethod
+  def urls_and_domains(cls, auth_entity, user_url, actor=None):
     """Returns this user's valid (not webmention-blocklisted) URLs and domains.
 
     Converts the auth entity's user_json to an ActivityStreams actor and uses
@@ -625,7 +626,7 @@ class Source(StringIdModel, metaclass=SourceMeta):
       ([string url, ...], [string domain, ...])
     """
     if not actor:
-      actor = self.gr_source.user_to_actor(json_loads(auth_entity.user_json))
+      actor = cls.gr_source.user_to_actor(json_loads(auth_entity.user_json))
     logging.debug(f'Extracting URLs and domains from actor: {json_dumps(actor, indent=2)}')
 
     candidates = util.trim_nulls(util.uniquify(
@@ -636,7 +637,7 @@ class Source(StringIdModel, metaclass=SourceMeta):
 
     urls = []
     for i, url in enumerate(candidates):
-      resolved = self.resolve_profile_url(url, resolve=i < MAX_AUTHOR_URLS)
+      resolved = cls.resolve_profile_url(url, resolve=i < MAX_AUTHOR_URLS)
       if resolved:
         urls.append(resolved)
 
@@ -646,7 +647,7 @@ class Source(StringIdModel, metaclass=SourceMeta):
       # skip links on this source's domain itself. only currently needed for
       # Mastodon; the other silo domains are in the webmention blocklist.
       domain = util.domain_from_link(url)
-      if domain != self.gr_source.DOMAIN:
+      if domain != cls.gr_source.DOMAIN:
         final_urls.append(url)
         domains.append(domain)
 

blogger.py
@@ -70,7 +70,7 @@ class Blogger(models.Source):
       auth_entity: :class:`oauth_dropins.blogger.BloggerV2Auth`
       blog_id: which blog. optional. if not provided, uses the first available.
     """
-    urls, domains = Blogger._urls_and_domains(auth_entity, blog_id=blog_id)
+    urls, domains = Blogger.urls_and_domains(auth_entity, blog_id=blog_id)
     if not urls or not domains:
       flash('Blogger blog not found. Please create one first!')
       return None
@@ -93,7 +93,7 @@ class Blogger(models.Source):
                    **kwargs)
 
   @staticmethod
-  def _urls_and_domains(auth_entity, blog_id=None):
+  def urls_and_domains(auth_entity, blog_id=None):
     """Returns an auth entity's URL and domain.
 
     Args:

medium.py
@@ -105,7 +105,7 @@ class Medium(models.Source):
       if pub.get('id') == id:
         return pub
 
-  def _urls_and_domains(self, auth_entity, user_url):
+  def urls_and_domains(self, auth_entity, user_url):
     if self.url:
       return [self.url], [util.domain_from_link(self.url)]
 

tests/test_browser.py
@@ -212,6 +212,29 @@ class BrowserViewTest(testutil.AppTest):
     self.assertEqual(400, resp.status_code)
     self.assertIn('Your FakeSource account is private. ', resp.get_data(as_text=True))
 
+  def test_profile_actor_urls(self):
+    FakeBrowserSource.gr_source.actor.update({
+      'url': 'https://patreon.com/bar',  # blocklisted
+      'urls': [
+        {'value': 'http:/fa.ke/foo'},  # silo profile
+        {'value': 'https://snarfed.org/'},
+        {'value': 'http://another.com/me'},
+        {'value': 'https://snarfed.org/'},  # duplicate
+      ],
+    })
+
+    self.expect_requests_get('http://another.com', '')
+    self.expect_requests_get('http://another.com', '')
+    self.expect_requests_get('https://snarfed.org/', '')
+    self.mox.ReplayAll()
+
+    resp = self.post('profile?token=towkin')
+    self.assertEqual(200, resp.status_code)
+
+    src = self.source.get()
+    self.assertEqual(['https://snarfed.org/', 'http://another.com/me'], src.domain_urls)
+    self.assertEqual(['snarfed.org', 'another.com'], src.domains)
+
   def test_profile_missing_token(self):
     resp = self.post('profile', auth=False)
     self.assertEqual(400, resp.status_code)
@@ -221,13 +244,13 @@ class BrowserViewTest(testutil.AppTest):
     self.domain.delete()
     resp = self.post('profile?token=towkin')
     self.assertEqual(403, resp.status_code)
-    self.assertEqual("Token towkin is not authorized for any of: {'snarfed.org'}",
+    self.assertEqual("Token towkin is not authorized for any of: ['snarfed.org']",
                      resp.get_data(as_text=True))
 
   def test_profile_bad_token(self):
     resp = self.post('profile?token=nope')
     self.assertEqual(403, resp.status_code)
-    self.assertEqual("Token nope is not authorized for any of: {'snarfed.org'}",
+    self.assertEqual("Token nope is not authorized for any of: ['snarfed.org']",
                      resp.get_data(as_text=True))
 
   def test_feed(self):

tumblr.py
@@ -87,9 +87,9 @@ class Tumblr(models.Source):
 
     Args:
       auth_entity: :class:`oauth_dropins.tumblr.TumblrAuth`
-      blog_name: which blog. optional. passed to _urls_and_domains.
+      blog_name: which blog. optional. passed to urls_and_domains.
     """
-    urls, domains = Tumblr._urls_and_domains(auth_entity, blog_name=blog_name)
+    urls, domains = Tumblr.urls_and_domains(auth_entity, blog_name=blog_name)
     if not urls or not domains:
       flash('Tumblr blog not found. Please create one first!')
       return None
@@ -105,7 +105,7 @@ class Tumblr(models.Source):
                   **kwargs)
 
   @staticmethod
-  def _urls_and_domains(auth_entity, blog_name=None):
+  def urls_and_domains(auth_entity, blog_name=None):
     """Returns this blog's URL and domain.
 
     Args:

wordpress_rest.py
@@ -88,7 +88,7 @@ class WordPress(models.Source):
                      site_info=site_info,
                      **kwargs)
 
-  def _urls_and_domains(self, auth_entity):
+  def urls_and_domains(self, auth_entity):
     """Returns this blog's URL and domain.
 
     Args:

commit dbbf3eded905d898a1386c833ecb96a625e9d96e

fix Source.urls_and_domains() bug, can't be a class method after all
introduced by 323b2b52ef857ea2ab86d3be9e4166c1dee597e8. fixes https://console.cloud.google.com/errors/detail/CKyc0-Gis8_5-QE

browser.py
@@ -156,21 +156,22 @@ class BrowserView(View):
       self.error(f'Your {self.gr_source().NAME} account is private. Bridgy only supports public accounts.')
 
     token = request.values['token']
-    urls, domains = self.source_class().urls_and_domains(None, None, actor=actor)
-    if not domains:
+    # create temporary source instance to get domains from actor
+    src = self.source_class().new(actor=actor)
+    if not src.domains:
       self.error(f'No usable web sites found in your {self.gr_source().NAME} profile. Add one of your registered domains above!')
 
 
     # update actor so resolved URLs can be reused
     actor.pop('url', None)
-    actor['urls'] = [{'value': url} for url in urls]
+    actor['urls'] = [{'value': url} for url in src.domain_urls]
 
-    logging.info(f'Checking token against domains {domains}')
-    for domain in ndb.get_multi(ndb.Key(Domain, d) for d in domains):
+    logging.info(f'Checking token against domains {src.domains}')
+    for domain in ndb.get_multi(ndb.Key(Domain, d) for d in src.domains):
       if domain and token in domain.tokens:
         return actor
 
-    self.error(f'Found link(s) to {domains} in your {self.gr_source().NAME} profile. Add one of your registered domains above!')
+    self.error(f'Found link(s) to {src.domains} in your {self.gr_source().NAME} profile. Add one of your registered domains above!')
 
   def auth(self):
     """Loads the source and token and checks that they're valid.

models.py
@@ -609,8 +609,7 @@ class Source(StringIdModel, metaclass=SourceMeta):
 
     self.put()
 
-  @classmethod
-  def urls_and_domains(cls, auth_entity, user_url, actor=None):
+  def urls_and_domains(self, auth_entity, user_url, actor=None):
     """Returns this user's valid (not webmention-blocklisted) URLs and domains.
 
     Converts the auth entity's user_json to an ActivityStreams actor and uses
@@ -626,7 +625,7 @@ class Source(StringIdModel, metaclass=SourceMeta):
       ([string url, ...], [string domain, ...])
     """
     if not actor:
-      actor = cls.gr_source.user_to_actor(json_loads(auth_entity.user_json))
+      actor = self.gr_source.user_to_actor(json_loads(auth_entity.user_json))
     logging.debug(f'Extracting URLs and domains from actor: {json_dumps(actor, indent=2)}')
 
     candidates = util.trim_nulls(util.uniquify(
@@ -637,7 +636,7 @@ class Source(StringIdModel, metaclass=SourceMeta):
 
     urls = []
     for i, url in enumerate(candidates):
-      resolved = cls.resolve_profile_url(url, resolve=i < MAX_AUTHOR_URLS)
+      resolved = self.resolve_profile_url(url, resolve=i < MAX_AUTHOR_URLS)
       if resolved:
         urls.append(resolved)
 
@@ -647,7 +646,7 @@ class Source(StringIdModel, metaclass=SourceMeta):
       # skip links on this source's domain itself. only currently needed for
       # Mastodon; the other silo domains are in the webmention blocklist.
       domain = util.domain_from_link(url)
-      if domain != cls.gr_source.DOMAIN:
+      if domain != self.gr_source.DOMAIN:
         final_urls.append(url)
         domains.append(domain)
 