075e84528fed07be8e6b4af813741fef88163703
Ryan Barrett <git@ryanb.org>
2022-06-11 18:17:30 -0700

    reduce /cron/replace_poll_tasks's memory footprint with query projection

--- cron.py
+++ cron.py
@@ -41,7 +41,9 @@ class LastUpdatedPicture(StringIdModel):
 def replace_poll_tasks():
   """Finds sources missing their poll tasks and adds new ones."""
   now = util.now_fn()
-  queries = [cls.query(Source.features == 'listen', Source.status == 'enabled')
+  fields = [cls.last_polled, cls.last_poll_attempt, cls.rate_limited]
+  queries = [cls.query(Source.features == 'listen', Source.status == 'enabled',
+                       projection=fields)
              for cls in models.sources.values() if cls.AUTO_POLL]
   for source in itertools.chain(*queries):
     age = now - source.last_poll_attempt
