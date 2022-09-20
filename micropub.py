97fc8e60cf4e0c28e8d6fe7c94c469a8826bc6e5
Ryan Barrett <git@ryanb.org>
2022-09-19 21:18:24 -0700

    micropub: implement token lookup

--- micropub.py
+++ micropub.py
@@ -13,13 +13,27 @@ from oauth_dropins.webutil.util import json_dumps, json_loads
 from werkzeug.exceptions import HTTPException
 
 from flask_app import app
+from flickr import Flickr
+from github import GitHub
+from oauth_dropins.flickr import FlickrAuth
+from oauth_dropins.github import GitHubAuth
+from oauth_dropins.mastodon import MastodonAuth
+from oauth_dropins.twitter import TwitterAuth
+from mastodon import Mastodon
 from publish import PublishBase
 import models
+from twitter import Twitter
 import util
 import webmention
 
 logger = logging.getLogger(__name__)
 
+SOURCE_CLASSES = (
+  (Twitter, TwitterAuth, TwitterAuth.token_secret),
+  (Mastodon, MastodonAuth, MastodonAuth.access_token_str),
+  (GitHub, GitHubAuth, GitHubAuth.access_token_str),
+  (Flickr, FlickrAuth, FlickrAuth.token_secret),
+)
 RESERVED_PARAMS = ('access_token', 'action', 'q', 'url')
 RESERVED_PREFIX = 'mp-'
 
@@ -31,12 +45,31 @@ def remove_reserved(params):
 
 class Micropub(PublishBase):
   """Micropub endpoint."""
+
+  def load_source(self):
+    """Looks up the auth entity by the provided access token."""
+    auth = request.headers.get('Authorization')
+    if auth:
+      parts = auth.split(' ')
+      if len(parts) != 2 or parts[0] != 'Bearer':
+        return self.error('Unsupported token format in Authorization header', status=401)
+      token = parts[1]
+    else:
+      token = request.values.get('access_token')
+
+    if not token:
+      return self.error('No token found in Authorization header or access_token param',
+                        status=401)
+
+    for src_cls, auth_cls, prop in SOURCE_CLASSES:
+      auth_entity = auth_cls.query(prop == token).get()
+      if auth_entity:
+        return src_cls.query(src_cls.auth_entity == auth_entity.key).get()
+
+    return self.error('No user found with that token', status=401)
+
   def dispatch_request(self):
     logging.info(f'Params: {list(request.values.items())}')
-
-    # TODO: look up token
-    from tests import testutil
-    self.source = testutil.FakeSource.query().get()
 
     # Micropub query; currently only config is supported
     q = request.values.get('q')
@@ -44,6 +77,8 @@ class Micropub(PublishBase):
       return jsonify({})
     elif q:
       return self.error(error='not_implemented')
+
+    self.source = self.load_source()
 
     # handle input
     if request.is_json:
