c735df5e19c75bc191ba08974104e350ad9dbc13
Ryan Barrett <git@ryanb.org>
2022-09-24 15:42:02 -0700

    micropub: implement delete, refactor tests a bit

--- micropub.py
+++ micropub.py
@@ -38,7 +38,7 @@ RESERVED_PARAMS = ('access_token', 'action', 'q', 'url')
 RESERVED_PREFIX = 'mp-'
 
 
-def remove_reserved(params):
+def form_to_mf2(params):
   # this should really be k.removesuffix('[]'), but removesuffix is new in
   # Python 3.9, and Circle is on 3.7 right now
   return {k.rstrip('[]'): v for k, v in params.items()
@@ -48,19 +48,27 @@ def remove_reserved(params):
 class Micropub(PublishBase):
   """Micropub endpoint."""
 
+  def error(self, error, description, **kwargs):
+    return super().error(error=error,
+                         extra_json={'error_description': description},
+                         **kwargs)
+
   def load_source(self):
     """Looks up the auth entity by the provided access token."""
     auth = request.headers.get('Authorization')
     if auth:
       parts = auth.split(' ')
       if len(parts) != 2 or parts[0] != 'Bearer':
-        return self.error('Unsupported token format in Authorization header', status=401)
+        return self.error('invalid_request',
+                          'Unsupported token format in Authorization header',
+                          status=401)
       token = parts[1]
     else:
       token = request.values.get('access_token')
 
     if not token:
-      return self.error('No token found in Authorization header or access_token param',
+      return self.error('unauthorized',
+                        'No token found in Authorization header or access_token param',
                         status=401)
 
     for src_cls, auth_cls, prop in SOURCE_CLASSES:
@@ -68,7 +76,7 @@ class Micropub(PublishBase):
       if auth_entity:
         return src_cls.query(src_cls.auth_entity == auth_entity.key).get()
 
-    return self.error('No user found with that token', status=401)
+    return self.error('unauthorized', 'No user found with that token', status=401)
 
   def dispatch_request(self):
     logging.info(f'Params: {list(request.values.items())}')
@@ -78,79 +86,94 @@ class Micropub(PublishBase):
     if q == 'config':
       return jsonify({})
     elif q:
-      return self.error(error='not_implemented')
+      return self.error('not_implemented', 'Only config query is supported')
 
     # now we assume it's a create/update/delete
     if request.method != 'POST':
-      return self.error('Expected POST for Micropub create/update/delete', status=405)
+      return self.error('invalid_request',
+                        'Expected POST for Micropub create/delete',
+                        status=405)
 
     self.source = self.load_source()
     if self.source.status == 'disabled' or 'publish' not in self.source.features:
-      return self.error(f'Publish is not enabled for {self.source.label()}',
+      return self.error('forbidden',
+                        f'Publish is not enabled for {self.source.label()}',
                         status=403)
 
     # handle input
     if request.is_json:
       mf2 = request.json
+      action = mf2.get('action')
+      url = mf2.get('url')
     elif request.form:
       mf2 = {
-        'h': request.values.get('h') or 'entry',
-        'properties': remove_reserved(request.form.to_dict(flat=False)),
+        'h': request.form.get('h') or 'entry',
+        'properties': form_to_mf2(request.form.to_dict(flat=False)),
       }
+      action = request.form.get('action')
+      url = request.form.get('url')
     elif request.files:
-      return self.error(error='not_implemented', extra_json={
-        'error_description': 'Multipart/file upload is not yet supported',
-      })
+      return self.error('not_implemented',
+                        'Multipart/file upload is not yet supported')
     else:
-      return self.error(error='invalid_request', extra_json={
-        'error_description': f'Unsupported Content-Type {request.content_type}',
-      })
+      return self.error('invalid_request',
+                        f'Unsupported Content-Type {request.content_type}')
+
+    if not action:
+      action = 'create'
+    if action not in ('create', 'delete'):
+      return self.error('not_implemented', f'Action {action} not supported')
 
     logging.debug(f'Got microformats2: {json_dumps(mf2, indent=2)}')
     obj = microformats2.json_to_object(mf2)
     logging.debug(f'Converted to ActivityStreams object: {json_dumps(obj, indent=2)}')
 
-    # done with the sanity checks, start publishing
+    canonicalized = self.source.URL_CANONICALIZER(url) or ''
+    post_id = self.source.gr_source.post_id(canonicalized)
+    if action == 'delete':
+      if not url:
+        return self.error('invalid_request', 'url is required for delete')
+      elif not canonicalized:
+        return self.error(
+          'invalid_request',
+          f"{url} doesn't look like a {self.source.gr_source.NAME} post URL")
+      elif not post_id:
+        return self.error(
+          'invalid_request',
+          f"Couldn't determine {self.source.gr_source.NAME} post id from {url}")
+
+    # done with validation, start publishing
     self.preprocess(obj)
-    self.entity = Publish(source=self.source.key, mf2=mf2)
+    type = 'delete' if action == 'delete' else None
+    self.entity = Publish(source=self.source.key, mf2=mf2, type=type)
     self.entity.put()
-    result = self.source.gr_source.create(obj)
+
+    if action == 'create':
+      result = self.source.gr_source.create(obj)
+    else:
+      assert action == 'delete'
+      assert post_id
+      result = self.source.gr_source.delete(post_id)
 
     logger.info(f'Result: {result}')
     if result.error_plain:
       self.entity.status = 'failed'
       self.entity.put()
-      return self.error(result.error_plain)
+      return self.error('failed', result.error_plain)
 
     self.entity.published = result.content
     self.entity.type = self.entity.published.get('type') or models.get_type(obj)
     self.entity.put()
 
-    # except HTTPException:
-    #   # raised by us, probably via self.error()
-    #   raise
-    # except BaseException as e:
-    #   code, body = util.interpret_http_exception(e)
-    #   if code in self.source.DISABLE_HTTP_CODES or isinstance(e, models.DisableSource):
-    #     # the user deauthorized the bridgy app, or the token expired, so
-    #     # disable this source.
-    #     logging.warning(f'Disabling source due to: {e}', exc_info=True)
-    #     self.source.status = 'disabled'
-    #     self.source.put()
-    #   if isinstance(e, (NotImplementedError, ValueError, urllib.error.URLError)):
-    #     code = '400'
-    #   elif not code:
-    #     raise
-    #   msg = f"Error: {body or ''} {e}"
-    #   return self.error(msg, status=code, report=code not in
-    #                     ('400', '403', '404', '406', '502', '503', '504'))
-
     # write results to datastore
-    self.entity.status = 'complete'
+    self.entity.status = 'complete' if action == 'create' else 'deleted'
     self.entity.put()
 
     url = self.entity.published.get('url')
-    return '', 201, ({'Location': url} if url else {})
+    if action == 'create':
+      return '', 201, ({'Location': url} if url else {})
+    else:
+      return '', 204
 
 
 app.add_url_rule('/micropub', view_func=Micropub.as_view('micropub'), methods=['GET', 'POST'])
