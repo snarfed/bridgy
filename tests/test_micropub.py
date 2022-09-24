60299dc8687561dfbf5e999d52afcaafa9f45f7a
Ryan Barrett <git@ryanb.org>
2022-09-23 20:56:23 -0700

    micropub: more tests

--- tests/test_micropub.py
+++ tests/test_micropub.py
@@ -51,6 +51,7 @@ class MicropubTest(AppTest):
       'granary_message': 'granary message',
       **kwargs,
     }, publish.published)
+    return publish
 
   def test_query_config(self):
     resp = self.client.get('/micropub?q=config')
@@ -93,59 +94,14 @@ class MicropubTest(AppTest):
     })
 
   def test_create_form_encoded(self):
-    resp = self.assert_response(data={
+    self.assert_response(data={
       'h': 'entry',
       'content': 'foo bar baz',
     })
     self.check_entity()
 
-  def test_create_form_encoded_single_photo(self):
-    resp = self.assert_response(data={
-      'h': 'entry',
-      'content': 'foo bar baz',
-      'photo': 'http://img',
-    })
-    self.check_entity(images=['http://img'])
-
-  def test_create_form_encoded_multiple_photos(self):
-    resp = self.assert_response(data=MultiDict((
-      ('h', 'entry'),
-      ('content', 'foo bar baz'),
-      ('photo[]', 'http://img'),
-      ('photo[]', 'http://other'),
-    )))
-    self.check_entity(images=['http://img', 'http://other'])
-
-#   def test_create_form_encoded_photo_url(self):
-# Content-type: application/x-www-form-urlencoded; charset=utf-8
-
-# h=entry
-# content=Micropub+test+of+creating+a+photo+referenced+by+URL
-# photo=http://TODO
-
-#   def test_create_form_encoded_reply(self):
-# Content-type: application/x-www-form-urlencoded; charset=utf-8
-
-# h=entry
-# content=Micropub+test+of+creating+an+h-entry+with+one+category.+This+post+should+have+one+category,+test1
-# category=test1
-
-#   def test_create_form_encoded_like(self):
-# Content-type: application/x-www-form-urlencoded; charset=utf-8
-
-# h=entry
-# content=Micropub+test+of+creating+an+h-entry+with+one+category.+This+post+should+have+one+category,+test1
-# category=test1
-
-#   def test_create_form_encoded_repost(self):
-# Content-type: application/x-www-form-urlencoded; charset=utf-8
-
-# h=entry
-# content=Micropub+test+of+creating+an+h-entry+with+one+category.+This+post+should+have+one+category,+test1
-# category=test1
-
   def test_create_json(self):
-    resp = self.assert_response(json={
+    self.assert_response(json={
       'type': ['h-entry'],
       'properties': {
         'content': ['foo bar baz'],
@@ -153,82 +109,102 @@ class MicropubTest(AppTest):
     })
     self.check_entity()
 
-#   def test_create_json_html_content(self):
-# {
-#   "type": ["h-entry"],
-#   "properties": {
-#     "content": [{
-#       "html": "TODO"
-#     }]
-#   }
-# }
+  def test_create_json_html_content(self):
+    self.assert_response(json={
+      'type': ['h-entry'],
+      'properties': {
+        'content': [{
+          'html': """\
+foo
+<em>bar</em>
+<div class="xyz"><p>baz</p></div>
+""",
+        }],
+      },
+    })
+    self.check_entity(content='foo _bar_\n\nbaz')
 
-#   def test_create_json_photo_url(self):
-# {
-#   "type": ["h-entry"],
-#   "properties": {
-#     "content": ["Micropub test of creating a photo referenced by URL. This post should include a photo of a sunset."],
-#     "photo": ["media/sunset.jpg"]
-#   }
-# }
+  def test_create_form_encoded_single_photo_url(self):
+    self.assert_response(data={
+      'h': 'entry',
+      'content': 'foo bar baz',
+      'photo': 'http://img',
+    })
+    self.check_entity(images=['http://img'])
+
+  def test_create_json_single_photo_url(self):
+    self.assert_response(json={
+      'type': ['h-entry'],
+      'properties': {
+        'content': ['foo bar baz'],
+        'photo': ['http://img'],
+      },
+    })
+    self.check_entity(images=['http://img'])
+
+  def test_create_form_encoded_multiple_photo_urls(self):
+    self.assert_response(data=MultiDict((
+      ('h', 'entry'),
+      ('content', 'foo bar baz'),
+      ('photo[]', 'http://img'),
+      ('photo[]', 'http://other'),
+    )))
+    self.check_entity(images=['http://img', 'http://other'])
+
+  def test_create_json_multiple_photo_urls(self):
+    self.assert_response(json={
+      'type': ['h-entry'],
+      'properties': {
+        'content': ['foo bar baz'],
+        'photo': ['http://img', 'http://other'],
+      }
+    })
+    self.check_entity(images=['http://img', 'http://other'])
+
+  # def test_create_json_photo_alt_text(self):
+  #   self.assert_response(json={
+  #     'type': ['h-entry'],
+  #     'properties': {
+  #       'content': ['foo bar baz'],
+  #       'photo': [{
+  #         'value': 'http://img',
+  #         'alt': 'an img',
+  #       }],
+  #     },
+  #   })
+  #   entity = self.check_entity(images=['http://img'])
 
 #   def test_create_json_nested_checkin(self):
 # {
-#     "type": [
-#         "h-entry"
+#     'type': [
+#         'h-entry'
 #     ],
-#     "properties": {
-#         "published": [
-#             "2017-05-31T12:03:36-07:00"
+#     'properties': {
+#         'published': [
+#             '2017-05-31T12:03:36-07:00'
 #         ],
-#         "content": [
-#             "Lunch meeting"
+#         'content': [
+#             'Lunch meeting'
 #         ],
-#         "checkin": [
+#         'checkin': [
 #             {
-#                 "type": [
-#                     "h-card"
+#                 'type': [
+#                     'h-card'
 #                 ],
-#                 "properties": {
-#                     "name": ["Los Gorditos"],
-#                     "url": ["https://foursquare.com/v/502c4bbde4b06e61e06d1ebf"],
-#                     "latitude": [45.524330801154],
-#                     "longitude": [-122.68068808051],
-#                     "street-address": ["922 NW Davis St"],
-#                     "locality": ["Portland"],
-#                     "region": ["OR"],
-#                     "country-name": ["United States"],
-#                     "postal-code": ["97209"]
+#                 'properties': {
+#                     'name': ['Los Gorditos'],
+#                     'url': ['https://foursquare.com/v/502c4bbde4b06e61e06d1ebf'],
+#                     'latitude': [45.524330801154],
+#                     'longitude': [-122.68068808051],
+#                     'street-address': ['922 NW Davis St'],
+#                     'locality': ['Portland'],
+#                     'region': ['OR'],
+#                     'country-name': ['United States'],
+#                     'postal-code': ['97209']
 #                 }
 #             }
 #         ]
 #     }
-# }
-
-#   def test_create_json_multiple_photo_urls(self):
-# {
-#   "type": ["h-entry"],
-#   "properties": {
-#     "content": ["Micropub test of creating multiple photos referenced by URL. This post should include a photo of a city at night."],
-#     "photo": [
-#       "media/sunset.jpg",
-#       "media/city-at-night.jpg"
-#     ]
-#   }
-# }
-
-#   def test_create_json_photo_alt_text(self):
-# {
-#   "type": ["h-entry"],
-#   "properties": {
-#     "content": ["Micropub test of creating a photo referenced by URL with alt text. This post should include a photo of a sunset."],
-#     "photo": [
-#       {
-#         "value": "media/sunset.jpg",
-#         "alt": "Photo of a sunset"
-#       }
-#     ]
-#   }
 # }
 
   # def test_create_multipart_photo(self):
