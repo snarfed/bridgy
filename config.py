33808416f90889db13a3b056139667ec1f179fa5
Ryan Barrett <git@ryanb.org>
2022-09-26 12:47:31 -0700

    tighten Flask cookie security for flashed messages
    ...now that we're rendering Micropub tokens in flashed messages.
    
    Background: https://flask.palletsprojects.com/en/2.2.x/security/?highlight=security#set-cookie-options

--- config.py
+++ config.py
@@ -4,6 +4,14 @@ https://flask.palletsprojects.com/en/latest/config/
 """
 from oauth_dropins.webutil import appengine_info, util
 
+# This is primarily for flashed messages, since we don't use session data
+# otherwise.
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+# Not strict because we flash messages after cross-site redirects for OAuth,
+# which strict blocks.
+SESSION_COOKIE_SAMESITE = 'Lax'
+
 if appengine_info.DEBUG:
   ENV = 'development'
   CACHE_TYPE = 'NullCache'
