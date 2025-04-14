9af35d1d2356b69aab530b97d1ed8639d56ffed5
Ryan Barrett <git@ryanb.org>
2025-04-14 12:46:35 -0700

    Source.get_activities_response: narrow https => tag id conversion
    for #1913

--- models.py
+++ models.py
@@ -433,12 +433,12 @@ class Source(StringIdModel, metaclass=SourceMeta):
     # https://github.com/snarfed/bridgy/issues/1913
     def convert_to_tag_ids(obj):
       if isinstance(obj, dict):
-        if id := obj.get('id'):
-          post_id = obj.get('numeric_id') or self.post_id(id)
+        if (id := obj.get('id')) and (id.startswith('http:')
+                                      or id.startswith('https:')):
           if (as1.object_type(obj) in as1.ACTOR_TYPES
               and (username := obj.get('username'))):
             obj['id'] = self.gr_source.tag_uri(username)
-          elif post_id and (id.startswith('http:') or id.startswith('https:')):
+          elif post_id := obj.get('numeric_id') or self.post_id(id):
             obj['id'] = self.gr_source.tag_uri(post_id)
 
         for val in obj.values():
